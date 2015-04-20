afc75e928d8366b0d6b0d35d82cdcd97cf89320c
jonathandick <jonathan.j.dick@gmail.com>
2015-04-20 18:56:25 +0300

    added query builder

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var Good = require('good');
 var Basic = require('hapi-auth-basic');
 var http = require('http');
 var settings = require('./settings.js');
+var squel = require ('squel');
+var _ = require('underscore');
 
 var server = new Hapi.Server(
     {connections: {
@@ -44,7 +46,48 @@ var validate = function (username,password,callback) {
 
 };
 
+//order=columname|asc,columnName|desc
 
+var getSortOrder = function(param) {
+    if(!param) return null;
+    console.log(param);
+    param = param.split(',')
+    var parts;
+    var order = [];
+    _.each(param,function(order_by) {
+        parts = order_by.split('|');
+        order.push({column:parts[0],asc:(parts[1].toLowerCase() === "asc")});
+    })
+    return order;
+}
+
+var queryLimit = 20;
+var queryOffset = 0;
+var queryServer = function(queryParts,callback) {
+    var s = squel.select()
+        .from(queryParts['table'])
+
+    s.where.apply(this,queryParts['where']);
+    _.each(queryParts['order'], function(param) {s.order(param.column, param.asc);});
+    s.limit(queryParts['limit'] || queryLimit);
+    s.offset(queryParts['offset'] || queryOffset)
+    var q = s.toParam();
+    console.log(q);
+
+    pool.getConnection(function (err, connection) {
+        if(err) {
+            console.log('Database Connection Error');
+            return;
+        }
+        connection.query(q.text, q.values,
+            function (err, rows, fields) {
+                connection.release();
+                if(err) console.log(err);
+                callback(rows);
+            });
+    });
+
+}
 
 server.register([
     {register: Basic, options:{}},
@@ -66,11 +109,12 @@ server.register([
             throw err; // something bad happened loading the plugin
         }
         server.auth.strategy('simple', 'basic', { validateFunc: validate });
+
         server.route({
             method: 'GET',
             path: '/',
             config: {
-                auth: 'simple',
+                //auth: 'simple',
                 handler: function(request,reply) {
                     reply('hello world');
                 }
@@ -81,16 +125,21 @@ server.register([
             method: 'GET',
             path: '/patient/{uuid}',
             config: {
-                auth: 'simple',
+                //auth: 'simple',
                 handler: function (request, reply) {
                     var uuid = request.params.uuid;
+                    console.log(request.params);
+                    var order = getSortOrder(request.query.order);
 
-                    pool.getConnection(function (err, connection) {
-                        connection.query('select * from reporting_JD.moh_data where uuid=' + uuid,
-                            function (err, rows, fields) {
-                                reply(rows);
-                            });
-                    });
+                    var queryParts = {
+                        table:"reporting_JD.moh_data",
+                        where:["uuid = ?",uuid],
+                        order: order || [{column:'encounter_datetime',asc:false}],
+                        offset:request.query.startIndex,
+                        limit:request.query.limit
+                    }
+
+                    queryServer(queryParts,reply);
                 }
             }
         });
