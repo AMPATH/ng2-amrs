1dc1585ca3c819ec30bbc19f8856ab201ac34989
jonathandick <jonathan.j.dick@gmail.com>
2015-05-04 10:56:56 +0300

    new version

--- etl-server.js
+++ etl-server.js
@@ -138,6 +138,48 @@ var queryServer = function(queryParts,callback) {
 
 }
 
+//str : code1 ## code2 ##
+function getARVNames(str) {
+    if(str === null || str === undefined) return "";
+    var arvs = {
+        814: "ABACAVIR",
+        817: "ABACAVIR LAMIVUDINE AND ZIDOVUDINE",
+        6159: "ATAZANAVIR",
+        6160: "ATAZANAVIR AND RITONAVIR",
+        796: "DIDANOSINE",
+        633: "EFAVIRENZ",
+        791: "EMTRICITABINE",
+        6679: "EPZICOM",
+        6158: "ETRAVIRINE",
+        749: "INDINAVIR",
+        6156: "ISENTRESS",
+        6965: "LAMIVIR S30",
+        628: "LAMIVUDINE",
+        1400: "LAMIVUDINE AND TENOFOVIR",
+        794: "LOPINAVIR AND RITONAVIR",
+        635: "NELFINAVIR",
+        631: "NEVIRAPINE",
+        6467: "NEVIRAPINE LAMIVUDINE AND ZIDOVUDINE",
+        1107: "NONE",
+        5424: "OTHER ANTIRETROVIRAL DRUG",
+        6157: "PREZISTA",
+        795: "RITONAVIR",
+        625: "STAVUDINE",
+        792: "STAVUDINE LAMIVUDINE AND NEVIRAPINE",
+        6964: "TDF AND 3TC AND EFV",
+        802: "TENOFOVIR",
+        6180: "TRUVADA",
+        5811: "UNKNOWN ANTIRETROVIRAL DRUG",
+        797: "ZIDOVUDINE",
+        630: "ZIDOVUDINE AND LAMIVUDINE"
+    }
+    var arvCodes = str.split(" ## ");
+    var arvNames = [];
+    _.each(arvCodes,function(code) {arvNames.push(arvs[code]);});
+    return arvNames.join(', ');
+}
+
+
 server.register([
     {register: Basic, options:{}},
     {
@@ -244,7 +286,7 @@ server.register([
             method: 'GET',
             path: '/etl/patient/{uuid}/hiv-summary',
             config: {
-                auth: 'simple',
+                //auth: 'simple',
                 handler: function (request, reply) {
                     var uuid = request.params.uuid;
                     var order = getSortOrder(request.query.order);
@@ -258,7 +300,13 @@ server.register([
                         limit:request.query.limit
                     }
 
-                    queryServer(queryParts,reply);
+                    queryServer(queryParts,function(result) {
+                        _.each(result.result,function(row) {
+                            row.cur_arv_meds = getARVNames(row.cur_arv_meds);
+                            row.arv_first_regimen = getARVNames(row.arv_first_regimen);
+                        })
+                        reply(result);
+                    });
                 }
             }
         });
@@ -297,7 +345,14 @@ server.register([
                         limit: request.query.limit
                     }
 
-                    queryServer(queryParts,reply);
+                    queryServer(queryParts,function(result) {
+                        _.each(result.result,function(row) {
+                            row.cur_arv_meds = getARVNames(row.cur_arv_meds);
+                            row.arv_first_regimen = getARVNames(row.arv_first_regimen);
+                        })
+                        reply(result);
+                    });
+
                 }
             }
         });
