c6403fa703b51054564532630412be6ead9b9261
kimaina <kimkenal@gmail.com>
2015-11-05 10:39:40 +0300

    AA-267: Removing FileStream Module Used In Loading Report Schema

--- etl-factory.js
+++ etl-factory.js
@@ -1,9 +1,8 @@
 "use strict";
 var _ = require('underscore');
-var fs = require('fs');
 //Report Indicators Json Schema Path
-var indicatorsSchemaPath = './reports/indicators.json';
-var reportSchemaPath = './reports/report-schema.json';
+var indicatorsSchema = require('./reports/indicators.json');
+var reports = require('./reports/report-schema.json');
 
 //etl-factory builds and generates queries dynamically in a generic way using indicator-schema and report-schema json files
 module.exports = function() {
@@ -13,45 +12,36 @@ module.exports = function() {
        if(queryParams  === null || queryParams === undefined) return "";
        var result="";
        //Load json schema into the query builder
-       fs.readFile(indicatorsSchemaPath, 'utf8', function (indicatorError, indicatorData) {
-        if (indicatorError) throw indicatorError; // we'll not consider error handling for now
-        var indicatorsSchema= JSON.parse(indicatorData);
-        fs.readFile(reportSchemaPath, 'utf8', function (reportError, reportData) {
-             if (reportError) throw reportError; // we'll not consider error handling for now
-             var reports=JSON.parse(reportData);
-                _.each(reports,function(report) {
-                    if(report.reportName===queryParams.reportName) {
-                        _.each(report.indicators, function (name) {
-                            console.log('here is the requested indicators', name);
-                            _.each(indicatorsSchema, function (indicator) {
-                                if (indicator.name === name) {
+        _.each(reports,function(report) {
+            if(report.reportName===queryParams.reportName) {
+                _.each(report.indicators, function (name) {
+                    console.log('here is the requested indicators', name);
+                    _.each(indicatorsSchema, function (indicator) {
+                        if (indicator.name === name) {
 
-                                        _.each(indicator.reportIndicators, function (reportIndicator) {
-                                            if (reportIndicator.name ===queryParams.countBy) {
-                                                result += ", ";
-                                                var str = reportIndicator.groupFunction + "( ";
-                                                if (reportIndicator.distinct === true)str += "distinct ";
+                                _.each(indicator.reportIndicators, function (reportIndicator) {
+                                    if (reportIndicator.name ===queryParams.countBy) {
+                                        result += ", ";
+                                        var str = reportIndicator.groupFunction + "( ";
+                                        if (reportIndicator.distinct === true)str += "distinct ";
 
-                                                if (reportIndicator.conditional === true) {
-                                                    str += "if(" + indicator.expression + "," +
-                                                        reportIndicator.fieldToCalulate + ",null)) as " + indicator.name;
-                                                }
-                                                else if (reportIndicator.conditional === false) {
-                                                    str += reportIndicator.fieldToCalulate + ") as " + indicator.name;
-                                                }
-                                                result += str.toString();
-                                            }
-                                        });
+                                        if (reportIndicator.conditional === true) {
+                                            str += "if(" + indicator.expression + "," +
+                                                reportIndicator.fieldToCalulate + ",null)) as " + indicator.name;
+                                        }
+                                        else if (reportIndicator.conditional === false) {
+                                            str += reportIndicator.fieldToCalulate + ") as " + indicator.name;
+                                        }
+                                        result += str.toString();
+                                    }
+                                });
 
-                                }
-                            });
-                        });
-                    }
-            });
-           successCallback(result);
-           });
-       });
-
+                        }
+                    });
+                });
+            }
+    });
+            successCallback(result);
         },
        buildPatientListExpression:function builPatientListExpression(queryParams, successCallback) {
             //Check for undefined params
@@ -62,51 +52,36 @@ module.exports = function() {
                 resource:""
             };
             //Load json schema into the query builder
-            fs.readFile(indicatorsSchemaPath, 'utf8', function (indicatorError, indicatorData) {
-                if (indicatorError) throw indicatorError; // we'll not handle error
-                //Parse Json Obj
-                var indicatorsSchema= JSON.parse(indicatorData);
-                    _.each(indicatorsSchema, function (indicator) {
-                        if (indicator.name === queryParams.reportIndicator) {
-                            if (indicator.expression != "") {
-                                result.whereClause= "and "+indicator.expression;
-                            }else{
-                                result.whereClause= indicator.expression;
-                            }
-                            result.resource=indicator.resource;
-                        }
-                    });
-                successCallback(result);
+            _.each(indicatorsSchema, function (indicator) {
+                if (indicator.name === queryParams.reportIndicator) {
+                    if (indicator.expression != "") {
+                        result.whereClause= "and "+indicator.expression;
+                    }else{
+                        result.whereClause= indicator.expression;
+                    }
+                    result.resource=indicator.resource;
+                }
             });
-
+           successCallback(result);
         },
        buildIndicatorsSchema:function buildIndicatorsSchema(queryParams, successCallback) {
             //Check for undefined params
             if(queryParams  === null || queryParams === undefined) return "";
             var result=[];
-            //Load json schema into the query builder
-            fs.readFile(indicatorsSchemaPath, 'utf8', function (indicatorError, indicatorData) {
-                if (indicatorError) throw indicatorError; // we'll not consider error handling for now
-                var indicatorsSchema= JSON.parse(indicatorData);
-                fs.readFile(reportSchemaPath, 'utf8', function (reportError, reportData) {
-                    if (reportError) throw reportError; // we'll not consider error handling for now
-                    var reports=JSON.parse(reportData);
-                    _.each(reports,function(report) {
-                        if(report.reportName===queryParams.reportName) {
-                            _.each(report.indicators, function (name) {
-                                console.log('here is the requested indicators', name);
-                                _.each(indicatorsSchema, function (indicator) {
-                                    if (indicator.name === name) {
-                                        result.push(indicator);
-                                    }
-                                });
-                            });
-                        }
+           //Load json schema into the query builder
+            _.each(reports,function(report) {
+                if(report.reportName===queryParams.reportName) {
+                    _.each(report.indicators, function (name) {
+                        console.log('here is the requested indicators', name);
+                        _.each(indicatorsSchema, function (indicator) {
+                            if (indicator.name === name) {
+                                result.push(indicator);
+                            }
+                        });
                     });
-                    successCallback(result);
-                });
+                }
             });
-
+            successCallback(result);
         }
     }
 }();
