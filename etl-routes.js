fa6b7228a656cd27a56dc18de56a67188c6d474b
kimaina <kimkenal@gmail.com>
2016-01-20 14:06:25 +0300

    AA-413:>Fixing MOH Locatioin From Taking Too Long To Load

--- etl-routes.js
+++ etl-routes.js
@@ -238,36 +238,25 @@ module.exports = function () {
             config: {
                 auth: 'simple',
                 handler: function (request, reply) {
-                    switch (request.query.report) {
-                        case 'MOH-731':
-                        {
-                            console.log("using this  case>>>>>> MOH-731")
+                    var asyncRequests = 0; //this should be the number of async requests needed before they are triggered
+                    var onResolvedPromise = function (promise) {
+                        asyncRequests--;
+                        if (asyncRequests <= 0) { //voting process to ensure all pre-processing of request async operations are complete
                             dao.getReportIndicators(request, reply);
-                            break;
                         }
-                        default:
-                        {
-                            var asyncRequests = 0; //this should be the number of async requests needed before they are triggered
-                            var onResolvedPromise = function (promise) {
-                                asyncRequests--;
-                                if (asyncRequests <= 0) { //voting process to ensure all pre-processing of request async operations are complete
-                                    dao.getReportIndicators(request, reply);
-                                }
-                            };
-                            if (request.query.locationUuids) {
-                                asyncRequests++;
-                            }
-                            if (asyncRequests == 0)
-                                dao.getReportIndicators(request, reply);
-                            if (request.query.locationUuids) {
-                                dao.getIdsByUuidAsyc('amrs.location', 'location_id', 'uuid', request.query.locationUuids,
-                                    function (results) {
-                                        request.query.locations = results;
-                                    }).onResolved = onResolvedPromise;
-                            }
-                            break;
-                        }
+                    };
+                    if (request.query.locationUuids) {
+                        asyncRequests++;
                     }
+                    if (asyncRequests == 0)
+                        dao.getReportIndicators(request, reply);
+                    if (request.query.locationUuids) {
+                        dao.getIdsByUuidAsyc('amrs.location', 'location_id', 'uuid', request.query.locationUuids,
+                            function (results) {
+                                request.query.locations = results;
+                            }).onResolved = onResolvedPromise;
+                    }
+
                 }
 
             }
