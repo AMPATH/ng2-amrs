dd6d4b6ce948d0a8c96b622dd54c9fcaab3fbecd
Emmanuel Nyachoke <achachiez@gmail.com>
2016-03-09 15:09:36 +0300

    ERS-4:Move etl properties that need to be configured into a configuration file

--- README.md
+++ README.md
@@ -1,5 +1,50 @@
-# etl-rest-server
-This is node project using hapi to expose rest endpoints providing access to data 
-hosted in ETL flat tables (The tables themselves are flattened derived tables 
-containing data from OpeMRS). There is a separate etl project that is responsible for
-data generation.
+This is node project using hapi to expose rest endpoints providing access to data hosted in ETL flat tables (The tables themselves are flattened derived tables containing data from OpeMRS). There is a separate etl project that is responsible for data generation. You can find the scripts [Here](https://github.com/AMPATH/etl). The project is currently being battle tested in [ng-amrs](https://github.com/AMPATH/ng-amrs).
+
+To setup the project run
+
+```$ git clone https://github.com/AMPATH/etl-rest-server.git```
+
+```$ cd etl-rest-server ```
+
+```$ npm install```
+
+```$ mkdir conf && cd conf```
+
+Create a config.json file
+
+```$ cat config.json```
+
+With the following content
+
+```json
+{
+  "openmrs": {
+    "host": "The IP or hostname of the OpenMRS server",
+    "port": 8080
+  },
+  "etl": {
+    "host": "The IP or hostname of the server running etl-rest-server",
+    "port": 8002,
+    "key": "path to the private key for tls",
+    "cert": "path to the public key for tls",
+    "tls": true
+  },
+  "mysql": {
+    "connectionLimit": 10,
+    "host": "The IP or hostname of the  mysql server",
+    "port": "3306",
+    "user": "<mysql user>",
+    "password": "<mysql password>",
+    "multipleStatements": true
+  }
+}
+
+```
+You can set ```tls:false``` if you don't care about https and don't provide the keys but if you set
+it to true you have to provide the keys.
+
+```npm start```
+
+Now visit ```https://<Your Host>:<Your Port>``` You should see the welcome message
+
+``` Welcome to ETL reset server for OpenMRS ```
--- etl-server.js
+++ etl-server.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var Good = require('good');
 var Basic = require('hapi-auth-basic');
 var https = require('https');
-var settings = require('./conf/settings.js');
+var config = require('./conf/config');
 var corsHeaders = require('hapi-cors-headers');
 var _ = require('underscore');
 var tls = require('tls');
@@ -11,99 +11,99 @@ var fs = require('fs');
 var routes = require('./etl-routes');
 var elasticRoutes = require('./elastic/routes/care.treatment.routes');
 
-var httpsServer = tls.createServer({
-    key: fs.readFileSync(settings.sslSettings.key),
-    cert: fs.readFileSync(settings.sslSettings.crt)
-});
-
 var server = new Hapi.Server({
-    connections: {
-        //routes: {cors:{origin:["https://amrs.ampath.or.ke:8443"]}}
-        routes: {
-            cors: {
-                additionalHeaders: ['JSNLog-RequestId']
-            }
-        }
+  connections: {
+    //routes: {cors:{origin:["https://amrs.ampath.or.ke:8443"]}}
+    routes: {
+      cors: {
+        additionalHeaders: ['JSNLog-RequestId']
+      }
     }
+  }
 });
-
+var https = false;
+if (config.etl.tls) {
+  https = tls.createServer({
+    key: fs.readFileSync(config.etl.key),
+    cert: fs.readFileSync(config.etl.cert)
+  });
+}
 
 server.connection({
-    port: 8002,
-    host:'localhost'
-    //tls: httpsServer
+  port: config.etl.port,
+  host: config.etl.host,
+  tls: https
 });
 
-
-var pool = mysql.createPool(settings.mysqlPoolSettings);
+var pool = mysql.createPool(config.mysql);
 
 var validate = function(username, password, callback) {
 
   //Openmrs context
   var options = {
-      hostname: 'amrs.ampath.or.ke',
-      port:8443,
-      path:'/amrs/ws/rest/v1/session',
-      headers: {
-          'Authorization': "Basic " + new Buffer(username + ":" + password).toString("base64")
-      }
-    };
+    hostname: config.openmrs.host,
+    port: config.openmrs.port,
+    path: '/amrs/ws/rest/v1/session',
+    headers: {
+      'Authorization': "Basic " + new Buffer(username + ":" + password).toString("base64")
+    }
+  };
 
-    https.get(options, function (res) {
-        var body = '';
-        res.on('data', function (chunk) {
-            body += chunk;
-        });
-        res.on('end', function () {
-            var result = JSON.parse(body);
-            console.log(result);
-            callback(null, result.authenticated, {});
-        });
-    }).on('error', function (error) {
-        console.log(error);
-        callback(null, false);
+  https.get(options, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
     });
+    res.on('end', function() {
+      var result = JSON.parse(body);
+      console.log(result);
+      callback(null, result.authenticated, {});
+    });
+  }).on('error', function(error) {
+    console.log(error);
+    callback(null, false);
+  });
 
 
 };
 
 
 server.register([{
-        register: Basic,
-        options: {}
-    }, {
-        register: Good,
-        options: {
-            reporters: [{
-                reporter: require('good-console'),
-                events: {
-                    response: '*',
-                    log: '*'
-                }
-            }]
+    register: Basic,
+    options: {}
+  }, {
+    register: Good,
+    options: {
+      reporters: [{
+        reporter: require('good-console'),
+        events: {
+          response: '*',
+          log: '*'
         }
-    }],
+      }]
+    }
+  }],
 
-    function (err) {
-        if (err) {
-            throw err; // something bad happened loading the plugin
-        }
-        server.auth.strategy('simple', 'basic', {
-            validateFunc: validate
-        });
-
-        //Adding routes
-        for (var route in routes) {
-            server.route(routes[route]);
-        }
-
-        for (var route in elasticRoutes) {
-            server.route(elasticRoutes[route]);
-        }
-
-        server.ext('onPreResponse', corsHeaders);
-        server.start(function () {
-            server.log('info', 'Server running at: ' + server.info.uri);
-        });
+  function(err) {
+    if (err) {
+      throw err; // something bad happened loading the plugin
+    }
+    server.auth.strategy('simple', 'basic', {
+      validateFunc: validate
     });
+
+    //Adding routes
+    for (var route in routes) {
+      server.route(routes[route]);
+    }
+
+    for (var route in elasticRoutes) {
+      server.route(elasticRoutes[route]);
+    }
+
+    server.ext('onPreResponse', corsHeaders);
+    server.start(function() {
+      server.log('info', 'Server running at: ' + server.info.uri);
+    });
+  });
 module.exports = server;
