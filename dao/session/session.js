7904ab5d6a486bbdcfec03089b6641acbf34320b
kimaina <kimkenal@gmail.com>
2016-08-12 12:53:03 +0300

    Add ability to invalidate user session stroed in memory

---
+++ dao/session/session.js
@@ -0,0 +1,30 @@
+/*jshint -W003, -W097, -W117, -W026 */
+'use strict';
+var db = require('../../etl-db');
+var _ = require('underscore');
+var cache = require('../../session-cache');
+
+
+module.exports = function () {
+
+    function invalidateUserSession(request, callback) {
+        if(request.headers.authorization) {
+            var header =request.headers.authorization;
+            var authBuffer=header.replace('Basic ','');
+            console.log(authBuffer);
+            cache.encriptKey(authBuffer, function (hash) {
+                cache.removeFromCache(hash);
+                callback('user session was invalidated successfully in etl server');
+            }, function (err) {
+                callback(Boom.badData(err));
+            });
+        } else{
+            callback(Boom.badData(err));
+        }
+    }
+
+
+    return {
+        invalidateUserSession: invalidateUserSession
+    }
+}();
