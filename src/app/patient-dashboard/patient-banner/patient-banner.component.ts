8d99c78521b0856d7b9b3dde5a944c5aa742ee22
Rodgers Tanui <rtankip@gmail.com>
2016-10-24 10:04:09 +0300

    Created the patient banner info

--- src/app/patient-dashboard/patient-banner/patient-banner.component.ts
+++ src/app/patient-dashboard/patient-banner/patient-banner.component.ts
@@ -9,6 +9,17 @@ import { Component, OnInit, Input } from '@angular/core';
 
 
 export class PatientBannerComponent implements OnInit {
+  //Assign the selected patient here
+  patient = {
+      fullName: "Test One Spens ",
+      gender: 'M',
+      dob: 1212345123,
+      age: 34,
+      ampathMrsUId: 163238471-3,
+      amrsMrn : 456789,
+      kenyaNationalId: 16150-2013
+  };
+
   constructor() { }
 
   ngOnInit() {
