1c42ad871f2acc12baeec0d6b5e28e7e694fcd10
Rodgers Tanui <rtankip@gmail.com>
2016-11-03 17:40:11 +0300

    NGPOC-26: Created patient info banner component

---
+++ src/app/patient-dashboard/patients.ts
@@ -0,0 +1,10 @@
+export class Patient {
+  fullName: string;
+  gender: string;
+  dob : number;
+  age: number;
+  ampathMrsUId: number;
+  amrsMrn : number;
+  cCC: number;
+  kenyaNationalId: number;
+}
