183dfc4323b021a569d23f62205e61b26598774d
BEN MOSONG <BEN MOSONG>
2016-11-16 14:28:00 +0300

    NGPOC-55: Create the clinical notes component as it is

---
+++ src/app/etl-api/clinical-notes-resource.service.spec.ts
@@ -0,0 +1,96 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Http, BaseRequestOptions, ResponseOptions, Response, RequestMethod } from '@angular/http';
+
+import { AppSettingsService } from '../app-settings/app-settings.service';
+import { LocalStorageService } from '../utils/local-storage.service';
+import { ClinicalNotesResourceService } from './clinical-notes-resource.service';
+
+
+describe('Clinical notes Resource Service Unit Tests', () => {
+
+  let backend: MockBackend, patientUuid = 'de662c03-b9af-4f00-b10e-2bda0440b03b';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backendInstance, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        ClinicalNotesResourceService,
+        AppSettingsService,
+        LocalStorageService
+      ]
+    });
+
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
+  });
+
+  it('should be injected with all dependencies',
+    inject([ClinicalNotesResourceService], (notesResourceService: ClinicalNotesResourceService) => {
+      expect(notesResourceService).toBeTruthy();
+    }));
+
+  it('should make API call with the correct url parameters', () => {
+
+    backend = TestBed.get(MockBackend);
+
+    backend.connections.subscribe((connection: MockConnection) => {
+
+      expect(connection.request.method).toBe(RequestMethod.Get);
+      expect(connection.request.url).toMatch('/patient/(*)/clinical-notes');
+      expect(connection.request.url).toContain('startIndex=');
+      expect(connection.request.url).toContain('limit=10');
+
+    });
+
+
+  });
+
+  it('should return the correct parameters from the api',
+    async(inject([ClinicalNotesResourceService, MockBackend],
+      (notesResourceService: ClinicalNotesResourceService, mockBackend: MockBackend) => {
+
+        let mockResponse = new Response(new ResponseOptions({
+          body: {
+            notes: [],
+            status: ''
+          }
+        }));
+
+        mockBackend.connections.subscribe(c => c.mockRespond(mockResponse));
+
+        notesResourceService.getClinicalNotes(patientUuid, 0, 10).subscribe((data) => {
+
+          expect(data).toBeTruthy();
+          expect(data.status).toBeDefined();
+          expect(data.notes).toBeDefined();
+          expect(data.notes.length).toEqual(0);
+
+        });
+
+      })));
+
+  it('should return the correct parameters from the api',
+    async(inject([ClinicalNotesResourceService, MockBackend],
+      (notesResourceService: ClinicalNotesResourceService, mockBackend: MockBackend) => {
+
+        mockBackend.connections.subscribe(c =>
+          c.mockError(new Error('An error occured while processing the request')));
+
+        notesResourceService.getClinicalNotes(patientUuid, 0, 10).subscribe((data) => { },
+          (error: Error) => {
+            expect(error).toBeTruthy();
+          });
+      })));
+
+});
--- src/app/patient-dashboard/clinical-notes/clinical-notes.component.ts
+++ src/app/patient-dashboard/clinical-notes/clinical-notes.component.ts
@@ -1,18 +1,132 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
 import { AppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytics.service';
+import { ClinicalNotesResourceService }  from '../../etl-api/clinical-notes-resource.service';
+import { ClinicalNotesHelperService } from './clinical-notes.helper';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-clinical-notes',
   templateUrl: './clinical-notes.component.html',
   styleUrls: ['./clinical-notes.component.css']
 })
-export class ClinicalNotesComponent implements OnInit {
+export class ClinicalNotesComponent implements OnInit, OnDestroy  {
 
-  constructor(private appFeatureAnalytics: AppFeatureAnalytics) {
+  dataLoaded: boolean = false;
+
+  hasNotes: boolean = false;
+
+  fetching: boolean = true;
+
+  experiencedLoadingError: boolean = false;
+
+  isBusy: Subscription;
+
+  notes: Array<any> = [];
+
+  private helper: ClinicalNotesHelperService;
+
+  private subscription: Subscription;
+
+  private nextStartIndex: number = 0;
+
+  private patientUuid: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private notesResource: ClinicalNotesResourceService,
+    private appFeatureAnalytics: AppFeatureAnalytics
+  ) {
+
+    this.helper = new ClinicalNotesHelperService();
   }
 
   ngOnInit() {
-    this.appFeatureAnalytics
-      .trackEvent('Patient Dashboard', 'Clinical Notes Loaded', 'ngOnInit');
+
+    this.appFeatureAnalytics.trackEvent('Patient Dashboard', 'Clinical Notes Loaded', 'ngOnInit');
+
+
+    this.subscription = this.route.parent.params.subscribe((params: Params) => {
+
+      this.patientUuid = params['patient_uuid'];
+
+      this.getNotes(0, 10, (err, notes) => {
+
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        this.notes = notes;
+
+        this.fetching = false;
+
+      });
+
+    });
+
+  }
+
+  ngOnDestroy() {
+
+    this.subscription.unsubscribe();
+    this.isBusy.unsubscribe();
+  }
+
+  getMoreNotes() {
+
+    this.nextStartIndex += this.notes.length;
+
+    this.fetching = false;
+
+    this.getNotes(this.nextStartIndex, 10, (err, notes) => {
+
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      this.fetching = false;
+
+      if (notes.length > 0) {
+        this.notes = this.notes.concat(notes);
+      } else {
+        this.dataLoaded = true;
+      }
+
+    });
+
+  }
+
+  getNotes(startIndex: number, limit: number, cb: Function) {
+
+    this.isBusy = this.notesResource.getClinicalNotes(
+      this.patientUuid,
+      startIndex,
+      limit
+    ).subscribe((data) => {
+
+      let _notes = data.notes;
+
+      if (_notes.length > 0) {
+
+        this.helper.format(_notes);
+
+        this.hasNotes = true;
+
+      }
+
+      cb(null, _notes);
+
+    }, (err) => {
+
+      this.experiencedLoadingError = true;
+
+      cb(err, null);
+
+    });
+
   }
 
 }
