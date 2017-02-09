2855f38e37ae3158d64920a55b42acb3e841e0fc
Nicky Kibor <nickykimaina@gmail.com>
2017-02-09 16:21:11 +0300

    Added scripts to support voiding of conflicting lab obs

---
+++ worker/extract-conflicts-script.js
@@ -0,0 +1,84 @@
+var fs = require('fs')
+    , util = require('util')
+    , stream = require('stream')
+    , moment = require('moment')
+    , es = require('event-stream');
+
+var lineNr = 0;
+
+var input_file = '../conflicts.txt';
+var output_file = '../processed-conflicts.csv';
+
+fs.exists(output_file, function (exists) {
+    if (exists) {
+        fs.unlink(output_file, function (err) {
+            if (err) throw err;
+            console.log('successfully deleted ' + output_file);
+        });
+    }
+});
+
+var s = fs.createReadStream(input_file)
+    .pipe(es.split())
+    .pipe(es.mapSync(function (line) {
+
+        // pause the readstream
+        s.pause();
+
+        lineNr += 1;
+
+        // if (lineNr < 20) {
+        try {
+            var lineArray = JSON.parse(line);
+            if (Array.isArray(lineArray)) {
+                for (var i = 0; i < lineArray.length; i++) {
+                    var conflict = lineArray[i];
+
+                    if (conflict.obs.length > 0) {
+                        for (var j = 0; j < conflict.obs.length; j++) {
+                            var obs = conflict.obs[j];
+                            var toDelete = {};
+                            toDelete.dateCollected = conflict.eid.DateCollected;
+                            toDelete.finalResult = conflict.eid.FinalResult;
+                            toDelete.patientIdentifier = conflict.eid.PatientID;
+                            toDelete.orderNo = conflict.eid.OrderNo;
+                            toDelete.obsDatetime = moment(obs.obsDatetime).format('DD-MMM-YY');
+                            toDelete.uuid = obs.uuid;
+                            toDelete.display = obs.display;
+                            toDelete.value = obs.value && obs.value.uuid ? obs.value.uuid : obs.value;
+                            toDelete.patientUuid = obs.person.uuid;
+                            toDelete.encounterUuid = obs.encounter && obs.encounter.uuid ? obs.encounter.uuid : '';
+                            // console.log(toDelete);
+                            var csvLine = '';
+                            for (var o in toDelete) {
+                                if (csvLine !== '') {
+                                    csvLine += '\t';
+                                }
+                                csvLine += toDelete[o];
+                            }
+                            csvLine += '\r\n';
+
+                            fs.appendFileSync(output_file, csvLine);
+                        }
+                    }
+                }
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+
+        // }
+
+        // console.log('reading line ', lineNr);
+
+        // resume the readstream, possibly from a callback
+        s.resume();
+    })
+        .on('error', function () {
+            console.log('Error while reading file.');
+        })
+        .on('end', function () {
+            console.log('Read entire file.')
+        })
+    );
---
+++ worker/void-conflicting-obs-script.js
@@ -0,0 +1,79 @@
+var fs = require('fs')
+    , util = require('util')
+    , stream = require('stream')
+    , moment = require('moment')
+    , curl = require('curlrequest')
+    , es = require('event-stream');
+
+var https = require('http');
+var config = require('../conf/config');
+
+var lineNr = 0;
+
+var input_file = './obs-uuid.csv';
+var error_file = './not-voided.csv';
+
+fs.exists(error_file, function (exists) {
+    if (exists) {
+        fs.unlink(error_file, function (err) {
+            if (err) throw err;
+            console.log('successfully deleted ' + error_file);
+        });
+    }
+});
+
+var s = fs.createReadStream(input_file)
+    .pipe(es.split())
+    .pipe(es.mapSync(function (line) {
+        // pause the readstream
+        s.pause();
+
+        lineNr += 1;
+
+        try {
+            if (line && line !== '') {
+                console.log('voiding obs: ', line);
+                var openmrsAppName = config.openmrs.applicationName || 'amrs';
+                var url = 'https://' + config.openmrs.host + ':' + config.openmrs.port + '/' + openmrsAppName + '/ws/rest/v1/obs/' + line + '?!purge';
+
+                var usernamePass = config.eidSyncCredentials.username + ":" + config.eidSyncCredentials.password;
+                var auth = "Basic " + new Buffer(usernamePass).toString('base64');
+
+                var options = {
+                    url: url,
+                    headers: {
+                        'Authorization': auth
+                    },
+                    method: 'DELETE'
+                };
+
+                curl.request(options, function (err, parts) {
+
+                    if (err || (parts && JSON.parse(parts).error)) {
+                        console.log('error voiding: ' + line, err || JSON.parse(parts).error);
+                        fs.appendFileSync(error_file, line + '\r\n');
+                    } else {
+                        console.log('voided: ' + line);
+                    }
+
+                    // resume the readstream, possibly from a callback
+                    s.resume();
+
+                });
+            }
+
+
+
+        } catch (error) {
+            console.error(error);
+            // resume the readstream, possibly from a callback
+            s.resume();
+        }
+    })
+        .on('error', function () {
+            console.log('Error while reading file.');
+        })
+        .on('end', function () {
+            console.log('Read entire file.')
+        })
+    );
