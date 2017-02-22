c0154c8aa7d91c2cb77f7b1f9473f2fd9e314ec1
Rodgers Tanui <rtankip@gmail.com>
2017-02-22 09:55:18 +0300

    NGPOC-147: A service to cache data on the client

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -6,13 +6,17 @@ import {
 // Load the implementations that should be tested
 import { App } from './app.component';
 import { AppState } from './app.service';
+import { CacheService } from 'ionic-cache/ionic-cache';
+import { DataCacheService } from './shared/services/data-cache.service';
 
 describe('App', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
       AppState,
-      App
+      App,
+      DataCacheService,
+      CacheService
     ]}));
 
   it('should have a name', inject([ App ], (app: App) => {
