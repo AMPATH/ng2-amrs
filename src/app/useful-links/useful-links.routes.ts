2100bd8c0db72f01e1c2dae2b644e1a2984116d6
Ben Ronnie <webtrendz@live.com>
2017-02-27 16:46:10 +0300

    Useful links component
    tatta
    
    kkjdfjf
    
    sfbjsdfs

---
+++ src/app/useful-links/useful-links.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { UsefulLinksComponent } from './useful-links.component';
+
+export const USEFUL_LINKS_ROUTE: Routes = [
+  {
+    path: 'useful-links',
+    component: UsefulLinksComponent
+  }
+];
