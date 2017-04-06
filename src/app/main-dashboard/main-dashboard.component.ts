93eaa512e374268637e884303b330790d99b9a12
Sudarshan <st@worcester.edu>
2017-04-05 20:55:06 -0400

    NGPOC 184: When location is selected under User Default Properties, the location is updated in the username drop down menu

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -11,6 +11,8 @@ import { UserService } from '../openmrs-api/user.service';
 import { User } from '../models/user.model';
 import { LocalStorageService } from '../utils/local-storage.service';
 import { AppState } from '../app.service';
+import { UserDefaultPropertiesService
+} from '../user-default-properties/user-default-properties.service';
 
 declare let jQuery: any;
 
@@ -37,12 +39,13 @@ export class MainDashboardComponent implements OnInit, OnDestroy {
     private localStore: LocalStorageService,
     private dynamicRoutesService: DynamicRoutesService,
     private authenticationService: AuthenticationService,
-    private userService: UserService, private appState: AppState) {
+    private userDefaultSettingsService: UserDefaultPropertiesService,
+    private userService: UserService, private appState: AppState) { }
 
-  }
   ngOnDestroy() {
     this.appSubscription.unsubscribe();
   }
+
   ngOnInit() {
     // Work Around for min-height
     window.dispatchEvent(new Event('resize'));
@@ -57,8 +60,18 @@ export class MainDashboardComponent implements OnInit, OnDestroy {
       err => console.log(err),
       () => console.log('Completed'));
     this.user = this.userService.getLoggedInUser();
-    let location = this.localStore.getItem('userDefaultLocation' + this.user.display);
-    this.userLocation = JSON.parse(location) ? JSON.parse(location).display : undefined;
+    this.userDefaultSettingsService.locationSubject.subscribe((location) => {
+       if (location) {
+         this.userLocation = JSON.parse(location) ? JSON.parse(location).display : '';
+       } else {
+         let defaultLocation =
+           this.localStore.getItem('userDefaultLocation' + this.user.display);
+         this.userLocation =
+           JSON.parse(defaultLocation) ? JSON.parse(defaultLocation).display :
+             undefined;
+       }
+     });
+
     this.appSubscription = this.appState.setupIdleTimer(1000 * 60 * 30)
       .subscribe((status: { idle: boolean }) => {
         this.active = status.idle;
