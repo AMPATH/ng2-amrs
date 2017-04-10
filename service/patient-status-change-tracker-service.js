080b7fbe145d2c79b14e917f49fbafa5eb238960
kimaina <kimkenal@gmail.com>
2017-04-10 15:12:03 +0300

    ERS-172: Added endpoints & placeholders for Patient Status report

---
+++ service/patient-status-change-tracker-service.js
@@ -0,0 +1,32 @@
+const dao = require('../etl-dao');
+const Promise = require("bluebird");
+const Moment = require('moment');
+const _ = require('lodash');
+export class PatientStatusChangeTracker {
+
+    getAggregateReport(reportParams) {
+        let self = this;
+        return new Promise(function (resolve, reject) {
+            reportParams.groupBy='groupByEndDate';
+            Promise.join(dao.runReport(reportParams),
+                (results) => {
+                    resolve(results);
+                    //TODO Do some post processing
+                }).catch((errors) => {
+                reject(errors);
+            });
+        });
+    }
+    getPatientListReport(reportParams) {
+        let self = this;
+        return new Promise(function (resolve, reject) {
+            //TODO: Do some pre processing
+            Promise.join(dao.getPatientListReport(reportParams),
+                (results) => {
+                    resolve(results);
+                }).catch((errors) => {
+                reject(errors);
+            });
+        });
+    }
+}
