9adba797281237f64ee22137c41df2b3658d6595
makombe <kennedymakombe@gmail.com>
2017-04-18 14:25:53 +0300

    Enable reminder to display new CD4 lab result

--- src/app/patient-dashboard/patient-dashboard.component.ts
+++ src/app/patient-dashboard/patient-dashboard.component.ts
@@ -69,8 +69,21 @@ export class PatientDashboardComponent implements OnInit, OnDestroy {
             if (result.length > 0) {
               let content = '';
               for (let test of result) {
-                content = content +
-                  `${test.display}</p>`;
+                if (test.groupMembers) {
+                  for (let l of test.groupMembers) {
+                    if (l.uuid === '5538cd04-9852-40f8-88ba-c69da32e50eb') {
+                      content = content +
+                        `CD4%: ${l.value}</p>`;
+                    }
+                    if (l.uuid === 'f9424af5-1fd3-4a8f-8d43-7b098eb20ac3') {
+                      content = content +
+                        `CD4: ${l.value}</p>`;
+                    }
+                  }
+                }else {
+                  content = content +
+                    `${test.display}</p>`;
+                }
               }
               this.toastrService.info(content, 'New Data from Lab');
               // app feature analytics
