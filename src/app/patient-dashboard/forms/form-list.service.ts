d6d693a4864fd23d3979dc4f506f3f91128b0fd5
Nicky Kibor <nickykimaina@gmail.com>
2017-04-18 18:17:03 +0300

    Included v2.2 forms for clinical testing

--- src/app/patient-dashboard/forms/form-list.service.ts
+++ src/app/patient-dashboard/forms/form-list.service.ts
@@ -61,11 +61,39 @@ export class FormListService {
 
         _.each(unsortArray,
             (item) => {
-                if (item.published === true) {
+                if (this.isVersion22xForm(item) ||
+                    (!this.isToBeExcludedInVersion22xForm(item) &&
+                        item.published === true)) {
                     publishedOpenmrsForms.push(item);
                 }
             });
         return publishedOpenmrsForms;
+    }
+
+    isVersion22xForm(form) {
+        let toInclude = [
+            '4f6db4d0-1d27-4d2a-bad0-a04dee2c54fe',
+            'c9f3a707-c640-4e55-96e7-e5a6d0126844',
+            '98296b2f-4c15-46be-81c6-268cc9085d19',
+            '86ff76f4-6a20-4166-b65f-db7d4d3d104e',
+            '86e538cb-c8a0-4974-b709-dd5ccd804524',
+            'ce5cf699-b2da-4e2d-9f18-17d1ef7e68a0'
+        ];
+
+        return toInclude.indexOf(form.uuid) >= 0;
+    }
+
+    isToBeExcludedInVersion22xForm(form) {
+        let toExclude = [
+            '38f6f2c8-6cb0-4aa1-aa84-fd4904bd2143',
+            'b8c817b5-c8fd-4f3c-8558-bf989c905234',
+            '3b1e50a6-df61-4b8f-b37d-235032f2c645',
+            '25ac6008-3b65-4435-b5b6-a8f0f3dd2b0d',
+            '9416746d-1e9f-47e8-aa2d-035aab6f3824',
+            '81f92a8a-ff5c-415d-a34c-b5bdca2406be'
+        ];
+
+        return toExclude.indexOf(form.uuid)  >= 0;
     }
 
     processFavouriteForms(openmrsForms, favouriteForms) {
