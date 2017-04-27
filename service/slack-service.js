0539cfa2ffaeb81d080455d4bc79bd829ace7bb6
Emmanuel Nyachoke <achachiez@gmail.com>
2017-04-27 16:01:06 +0300

    Switch to using webapis as opposed to rtm api

--- service/slack-service.js
+++ service/slack-service.js
@@ -1,23 +1,20 @@
-import { RtmClient } from '@slack/client';
-const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const WebClient = require('@slack/client').WebClient;
 const config = require('../conf/config')
 const Promise = require("bluebird");
 export class SlackService {
     sendUserFeedBack(message) {
         let bot_token = process.env.SLACK_BOT_TOKEN || config.slack.bottoken;
         let feedbackChannel = config.slack.feedback_channel;
-        let rtm = new RtmClient(bot_token);
-        let channel;
-
         return new Promise(function (resolve, reject) {
-            rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-                rtm.sendMessage(message, 'G4GFP8BPU');
-                resolve({ status: 'ok' })
-            });
-
-            rtm.start();
-            rtm.on(CLIENT_EVENTS.RTM.UNABLE_TO_RTM_START, function () {
-                reject({ status: 'error' })
+            let web = new WebClient(bot_token);
+            web.chat.postMessage('G4GFP8BPU', 'Hello there', function (err, res) {
+                if (err) {
+                    console.log('Error:', err);
+                    resolve({ status: err });
+                } else {
+                    console.log('Message sent: ', res);
+                    resolve({ status: 'okay' });
+                }
             });
         });
     }
