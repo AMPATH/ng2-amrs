159baa5e479009b33511ba5b6221aacd2852f49c
Nicky Kibor <nickykimaina@gmail.com>
2017-06-08 16:26:32 +0300

    NGPOC-318: Fixed patient-lists uneven row height

--- src/app/data-lists/patient-list/patient-list-columns.data.ts
+++ src/app/data-lists/patient-list/patient-list-columns.data.ts
@@ -2,24 +2,26 @@ export class PatientListColumns {
 
     public static columns() {
         return [
-          {
-            headerName: '#',
-            width: 60,
-            pinned: true,
-            cellRenderer: (column) => {
-              return parseInt(column.node.id, 10) + 1;
+            {
+                headerName: '#',
+                width: 60,
+                pinned: true,
+                cellRenderer: (column) => {
+                    return parseInt(column.node.id, 10) + 1;
+                },
+                field: '#'
             },
-            field: '#'
-          },
             {
                 headerName: 'Identifiers',
                 field: 'identifiers',
                 width: 150,
                 cellStyle: {
-                    'white-space': 'normal'
+                    'white-space': 'nowrap',
+                    'text-overflow': 'ellipsis'
                 },
                 pinned: true,
-                filter: 'text'
+                filter: 'text',
+                cellClass: 'identifier-column'
             },
             {
                 headerName: 'Person Name',
