39bec84e541c46b5132c6bb366061803a0ff1d08
Nicky Kibor <nickykimaina@gmail.com>
2017-06-16 09:43:35 +0300

    NGPOC-357: fixed addresss issue causing app to bug out

--- src/app/patient-dashboard/patient-info/address.component.ts
+++ src/app/patient-dashboard/patient-info/address.component.ts
@@ -14,14 +14,14 @@ import { Subscription } from 'rxjs';
 export class AddressComponent implements OnInit, OnDestroy {
   patients: Patient = new Patient({});
   subscription: Subscription;
-  private address1: string ;
-  private address2: string ;
-  private address3: string ;
-  private cityVillage: string ;
-  private stateProvince: string ;
+  private address1: string;
+  private address2: string;
+  private address3: string;
+  private cityVillage: string;
+  private stateProvince: string;
 
   constructor(private patientService: PatientService,
-              ) { }
+  ) { }
   ngOnInit(): void {
     this.getPatient();
   }
@@ -38,11 +38,14 @@ export class AddressComponent implements OnInit, OnDestroy {
         this.patients = new Patient({});
         if (patient) {
           this.patients = patient;
-          this.address1 = (this.patients.person.preferredAddress as any).address1;
-          this.address2 = (this.patients.person.preferredAddress as any).address2;
-          this.address3 = (this.patients.person.preferredAddress as any).address3;
-          this.cityVillage = (this.patients.person.preferredAddress as any).cityVillage;
-          this.stateProvince = (this.patients.person.preferredAddress as any).stateProvince;
+
+          if (this.patients.person.preferredAddress !== null) {
+            this.address1 = (this.patients.person.preferredAddress as any).address1;
+            this.address2 = (this.patients.person.preferredAddress as any).address2;
+            this.address3 = (this.patients.person.preferredAddress as any).address3;
+            this.cityVillage = (this.patients.person.preferredAddress as any).cityVillage;
+            this.stateProvince = (this.patients.person.preferredAddress as any).stateProvince;
+          }
         }
       }
     );
