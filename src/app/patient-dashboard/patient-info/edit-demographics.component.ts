fa74b81c3f0aac07ae747ca569293f1e5d23fd88
Ben Ronnie <webtrendz@live.com>
2017-06-29 12:23:24 +0300

    APTS-436: Bug While Editing Deceased Patients

--- src/app/patient-dashboard/patient-info/edit-demographics.component.ts
+++ src/app/patient-dashboard/patient-info/edit-demographics.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PatientService } from '../patient.service';
 import { Patient } from '../../models/patient.model';
 import { PersonResourceService } from '../../openmrs-api/person-resource.service';
-import { ConceptResourceService  } from '../../openmrs-api/concept-resource.service';
+import { ConceptResourceService } from '../../openmrs-api/concept-resource.service';
 import { Subscription } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'edit-demographics',
@@ -21,22 +22,17 @@ export class EditDemographicsComponent implements OnInit, OnDestroy {
   private preferred: any;
   private ispreferred: boolean;
   private preferredOptions = [
-      { label: 'Yes', val: true },
-      { label: 'No', val: false }
+    {label: 'Yes', val: true},
+    {label: 'No', val: false}
   ];
   private genderOptions = [
-    { label: 'Female', val: 'F' },
-    { label: 'Male', val: 'M' }
+    {label: 'Female', val: 'F'},
+    {label: 'Male', val: 'M'}
   ];
   private preferredNameuuid: string;
   private birthdate: any;
-  private dead: any;
-  private isDead: boolean;
+  private dead: boolean = false;
   private gender: any;
-  private isDeadOptions = [
-      { label: 'Yes', val: true },
-      { label: 'No', val: false }
-  ];
   private deathDate: Date;
   private causesOfDeath: any = [];
   private causeOfDeath: any;
@@ -46,7 +42,9 @@ export class EditDemographicsComponent implements OnInit, OnDestroy {
 
   constructor(private patientService: PatientService,
               private personResourceService: PersonResourceService,
-              private conceptResourceService: ConceptResourceService) { }
+              private conceptResourceService: ConceptResourceService) {
+  }
+
   ngOnInit(): void {
     this.getPatient();
     this.getCauseOfDeath();
@@ -63,112 +61,112 @@ export class EditDemographicsComponent implements OnInit, OnDestroy {
       (patient) => {
         this.patients = new Patient({});
         if (patient) {
-            this.patients = patient;
-          console.log('patient', this.patients);
-            this.givenName = (this.patients.person.preferredName as any).givenName;
-            this.middleName = (this.patients.person.preferredName as any).middleName;
-            this.familyName = (this.patients.person.preferredName as any).familyName;
-            this.ispreferred = (this.patients.person.preferredName as any).preferred;
-            this.preferredNameuuid = (this.patients.person.preferredName as any).uuid;
-            this.isDead = false;
-            this.gender = this.patients.person.gender;
-            this.healthCenter = this.patients.person.healthCenter;
+          this.patients = patient;
+          this.givenName = (this.patients.person.preferredName as any).givenName;
+          this.middleName = (this.patients.person.preferredName as any).middleName;
+          this.familyName = (this.patients.person.preferredName as any).familyName;
+          this.ispreferred = (this.patients.person.preferredName as any).preferred;
+          this.preferredNameuuid = (this.patients.person.preferredName as any).uuid;
+          this.gender = this.patients.person.gender;
+          this.healthCenter = this.patients.person.healthCenter;
         }
       }
     );
   }
+
   showDialog() {
     this.display = true;
   }
+
   public dismissDialog() {
     this.display = false;
   }
-    public getCauseOfDeath() {
-        let conceptUid = 'a89df750-1350-11df-a1f1-0026b9348838';
-        this.conceptResourceService.getConceptByUuid(conceptUid).subscribe((data) => {
-            if (data) {
-                this.causesOfDeath = data.answers;
-            }
-        }, (error) => {
-            console.log('Failed to load concepts ', error);
-        });
+
+  public updateDeathDate(deathDate) {
+    this.deathDate = deathDate;
+  }
+
+  public getCauseOfDeath() {
+    let conceptUid = 'a89df750-1350-11df-a1f1-0026b9348838';
+    this.conceptResourceService.getConceptByUuid(conceptUid).subscribe((data) => {
+      if (data) {
+        this.causesOfDeath = data.answers;
+      }
+    }, (error) => {
+      console.log('Failed to load concepts ', error);
+    });
+  }
+
+  public updateName() {
+    this.errors = [];
+    this.successAlert = '';
+    if (this.familyName === '') {
+      this.errors.push({message: 'Family Name is required'});
     }
-    public updateName() {
-        this.errors = [];
-        this.successAlert = '';
-
-        if (this.familyName === '') {
-            this.errors.push( { message: 'Family Name is required'} );
-        }
-        if (this.givenName === '') {
-            this.errors.push( { message: 'Given Name is required'} );
-        }
-        if (this.preferred === '') {
-            this.errors.push( { message: 'Preferred is required'} );
-        }
-        if (this.preferred === 'true') {
-            this.ispreferred = true;
-        }
-        if (this.preferred === 'false') {
-            this.ispreferred = false;
-        }
-
-        if (this.dead === 'false') {
-            this.isDead = false;
-            this.deathDate = null;
-            this.causeOfDeath = null;
-
-        } else if (this.dead === 'true') {
-
-            if (this.deathDate == null ) {
-                this.errors.push( { message: 'Death Date is required'} );
-            }
-            if (this.causeOfDeath == null) {
-                this.errors.push( { message: 'Cause of Death is required'} );
-            }
-            if (this.dead === 'true') {
-                this.isDead = true;
-            }
-            if (this.dead === 'false') {
-                this.isDead = false;
-            }
-
-        }
-
-        if (this.errors.length === 0) {
-            let person = {
-                uuid: this.patients.person.uuid
-            };
-            let personNamePayload = {
-                names: [
-                {
-                    familyName: this.familyName,
-                    givenName: this.givenName,
-                    middleName: this.middleName,
-                    preferred: this.ispreferred,
-                    uuid : this.preferredNameuuid
-                }],
-                dead: this.isDead,
-                deathDate: this.deathDate,
-                causeOfDeath: this.causeOfDeath,
-                gender: this.gender
-
-            };
-            this.personResourceService.saveUpdatePerson(person.uuid, personNamePayload).subscribe(
-            (success) => {
-                if ( success ) {
-                this.patientService.fetchPatientByUuid(this.patients.person.uuid);
-                }
-               // this.successAlert = 'Successfully updated Patient Demographics';
-            },
-            (error) => {
-                console.log('error', error);
-                this.errors.push({
-                    message: 'error updating demographics'
-                });
-            }
-            );
-        }
-      this.display = false;
+    if (this.givenName === '') {
+      this.errors.push({message: 'Given Name is required'});
     }
+    if (this.preferred === '') {
+      this.errors.push({message: 'Preferred is required'});
+    }
+    if (this.preferred === 'true') {
+      this.ispreferred = true;
+    }
+    if (this.preferred === 'false') {
+      this.ispreferred = false;
+    }
+
+    if (!this.dead) {
+      this.deathDate = null;
+      this.causeOfDeath = null;
+
+    } else if (this.dead) {
+      if (moment(this.deathDate).isAfter(new Date())) {
+        this.errors.push({message: 'Date date cannot be in future'});
+      }
+      if (this.deathDate == null) {
+        this.errors.push({message: 'Death Date is required'});
+      }
+      if (this.causeOfDeath == null) {
+        this.errors.push({message: 'Cause of Death is required'});
+      }
+
+    }
+
+    if (this.errors.length === 0) {
+      let person = {
+        uuid: this.patients.person.uuid
+      };
+      let personNamePayload = {
+        names: [
+          {
+            familyName: this.familyName,
+            givenName: this.givenName,
+            middleName: this.middleName,
+            preferred: this.ispreferred,
+            uuid: this.preferredNameuuid
+          }],
+        dead: this.dead,
+        deathDate: this.deathDate,
+        causeOfDeath: this.causeOfDeath,
+        gender: this.gender
+
+      };
+      this.personResourceService.saveUpdatePerson(person.uuid, personNamePayload).subscribe(
+        (success) => {
+          if (success) {
+            this.patientService.fetchPatientByUuid(this.patients.person.uuid);
+            this.display = false;
+          }
+          // this.successAlert = 'Successfully updated Patient Demographics';
+        },
+        (error) => {
+          console.log('error', error);
+          this.errors.push({
+            message: 'error updating demographics'
+          });
+        }
+      );
+    }
+  }
 }
