8c26d27bd21252f8a68ae1a85fe2bbd8e9600f35
Nicky Kibor <nickykimaina@gmail.com>
2017-07-04 18:27:04 +0300

    ERS-199: Added CRUD methods for cohort_user

---
+++ dao/connection/mysql-connection-service.js
@@ -0,0 +1,26 @@
+/*jshint -W003, -W097, -W117, -W026 */
+'use strict';
+
+var mysql = require('mysql');
+var Promise = require('bluebird');
+var config = require('../../conf/config');
+
+ var pool = mysql.createPool(config.mysql);
+
+var def = {
+    getServerConnection: getServerConnection
+};
+
+module.exports = def;
+
+function getServerConnection() {
+    return new Promise(function (resolve, reject) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(connection);
+            }
+        });
+    });
+}
