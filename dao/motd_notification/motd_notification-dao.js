156b475a24c37fe9a57d0d081f880e2477b4d608
maikofelix89 <maikofelix89@gmail.com>
2017-07-11 10:06:35 +0300

    NGPOC-367: Created motd_table and endpoints to fetch mots messages/

---
+++ dao/motd_notification/motd_notification-dao.js
@@ -0,0 +1,55 @@
+/*jshint -W003, -W097, -W117, -W026 */
+'use strict';
+
+var Promise = require('bluebird');
+var squel = require('squel');
+var _ = require('underscore');
+var moment = require('moment');
+var connection = require('../../dao/connection/mysql-connection-service.js');
+var authorizer = require('../../authorization/etl-authorizer');
+
+var motd = {
+    getMotdNotifications : getMotdNotifications
+};
+
+module.exports = motd;
+
+function getMotdNotifications() {
+
+    return new Promise(function (resolve, reject) {
+            connection.getServerConnection()
+            .then(function (conn) {
+                   var query = squel.select()
+                    .field('mo.message_id')
+                    .field('mo.message')
+                    .field('mo.title')
+                    .field('mo.startDate')
+                    .field('mo.expireTime')
+                    .field('mo.dateCreated')
+                    .field('mo.alert_type')
+                    .field('mo.alert_interval')
+                    .from('etl.motd_messages', 'mo')
+                    .toString();
+                conn.query(query, {}, function (err, rows, fields) {
+                    if (err) {
+                        reject('Error querying server');
+                    }
+                    else {
+                        resolve(rows);
+                    }
+                    conn.release();
+                });
+            })
+            .catch(function (err) {
+                reject('Error establishing connection to MySql Server');
+            });
+
+
+
+    });
+   
+}
+
+
+
+
