1a82c5727a7a7285ae0bbae5ac67425a194bb696
makombe <kennedymakombe@gmail.com>
2017-08-31 19:56:20 +0300

    NGPOC-451: Added HIV monthly summary resource service

---
+++ src/app/etl-api/hiv-monthly-summary-indicators-resource.service.spec.ts
@@ -0,0 +1,171 @@
+import { TestBed, async, inject, fakeAsync } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import {
+    BaseRequestOptions, XHRBackend, Http, RequestMethod,
+    ResponseOptions, Response, URLSearchParams
+} from '@angular/http';
+import { LocalStorageService } from '../utils/local-storage.service';
+import { CacheModule, CacheService } from 'ionic-cache';
+import { AppSettingsService } from '../app-settings';
+import { DataCacheService } from '../shared/services/data-cache.service';
+import {
+  HivMonthlySummaryIndicatorsResourceService
+} from './hiv-monthly-summary-indicators-resource.service';
+
+const expectedHivMonthlySummaryIndicatorsResults = {
+    startIndex: 0,
+    size: 1,
+    result: [
+        {
+            location: 'location name',
+            location_uuid: 'location-uuid',
+            location_id: 13,
+            encounter_datetime: '2017-04-26T05:48:32.000Z',
+            month: '2017-04-26T05:48:32.000Z',
+            reporting_month: '04/2017',
+            on_arvs: 2889,
+        },
+        {
+          location: 'location name',
+          location_uuid: 'location-uuid',
+          location_id: 13,
+          encounter_datetime: '2017-04-26T05:48:32.000Z',
+          month: '2017-05-26T05:48:32.000Z',
+          reporting_month: '05/2017',
+          on_arvs: 2889,
+        }
+    ],
+    indicatorDefinitions: [{
+
+    }]
+};
+
+const reportParams = {
+    startIndex: undefined,
+    startDate: '2017-03-01',
+    locationUuids: '08fec056-1352-11df-a1f1-0026b9348838',
+    limit: undefined,
+    endDate: '2017-04-27',
+    gender: 'M,F',
+    indicators: 'on_arvs',
+    startAge: 0,
+    endAge: 110
+};
+
+const patientList = {
+    startIndex: 0,
+    size: 3,
+    result: [
+        {
+            person_id: 1817,
+            encounter_id: 6774060,
+            location_id: 13,
+            location_uuid: '08fec056-1352-11df-a1f1-0026b9348838',
+            patient_uuid: '5b737014-1359-11df-a1f1-0026b9348838',
+            gender: 'F',
+            birthdate: '1982-12-11T21:00:00.000Z',
+            age: 34
+
+        }
+    ]
+};
+
+describe('HivMonthlySummaryIndicatorsResourceService Tests', () => {
+    let service;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [],
+          imports: [CacheModule],
+            providers: [
+                HivMonthlySummaryIndicatorsResourceService,
+                MockBackend,
+                BaseRequestOptions,
+                AppSettingsService,
+                LocalStorageService,
+                CacheService,
+                DataCacheService,
+                {
+                    provide: Http,
+                    deps: [MockBackend, BaseRequestOptions],
+                    useFactory:
+                    (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                        return new Http(backend, defaultOptions);
+                    }
+                }
+            ]
+        });
+    });
+
+    it('should be defined',
+        inject([HivMonthlySummaryIndicatorsResourceService],
+            (s: HivMonthlySummaryIndicatorsResourceService) => {
+                expect(s).toBeTruthy();
+            })
+    );
+
+    it('Hiv monthly Summary Indicators resource service resource methods should be defined',
+        inject([HivMonthlySummaryIndicatorsResourceService],
+            (s: HivMonthlySummaryIndicatorsResourceService) => {
+                expect(s.getUrl).toBeDefined();
+                expect(s.getPatientListUrl).toBeDefined();
+                expect(s.getHivSummaryMonthlyIndicatorsReport).toBeDefined();
+                expect(s.getHivSummaryMonthlyIndicatorsPatientList).toBeDefined();
+            })
+    );
+
+    it('should return report urlRequest parameters',
+        inject([HivMonthlySummaryIndicatorsResourceService, MockBackend],
+            (s: HivMonthlySummaryIndicatorsResourceService, backend: MockBackend) => {
+                let urlParams = s.getUrlRequestParams(reportParams);
+                let params = urlParams.toString();
+                expect(params).toContain('locationUuids=08fec056-1352-11df-a1f1-0026b9348838');
+                expect(params).toContain('endDate=2017-04-27');
+                expect(params).toContain('gender=M,F');
+                expect(params).toContain('startDate=2017-03-01');
+                expect(params).toContain('indicators=on_arvs');
+                expect(params).toContain('startAge=0');
+                expect(params).toContain('endAge=110');
+
+            }
+        )
+    );
+
+    it('should return Hiv monthly Summary Indicators Report',
+        inject([HivMonthlySummaryIndicatorsResourceService, MockBackend],
+            (s: HivMonthlySummaryIndicatorsResourceService, backend: MockBackend) => {
+                backend.connections.subscribe((connection: MockConnection) => {
+                    expect(connection.request.method).toBe(RequestMethod.Get);
+                    connection.mockRespond(new Response(
+                        new ResponseOptions({
+                            body: expectedHivMonthlySummaryIndicatorsResults
+                        }
+                        )));
+                });
+
+                s.getHivSummaryMonthlyIndicatorsReport(reportParams).subscribe((result) => {
+                    expect(result).toBeDefined();
+                    expect(result).toEqual(expectedHivMonthlySummaryIndicatorsResults);
+                });
+            })
+    );
+
+    it('should return Hiv monthly Summary Indicators Report Patient List',
+        inject([HivMonthlySummaryIndicatorsResourceService, MockBackend],
+            (s: HivMonthlySummaryIndicatorsResourceService, backend: MockBackend) => {
+                backend.connections.subscribe((connection: MockConnection) => {
+                    expect(connection.request.method).toBe(RequestMethod.Get);
+                    connection.mockRespond(new Response(
+                        new ResponseOptions({
+                            body: patientList
+                        }
+                        )));
+                });
+
+                s.getHivSummaryMonthlyIndicatorsPatientList(reportParams).subscribe((result) => {
+                    expect(result).toBeDefined();
+                    expect(result).toEqual(patientList.result);
+                });
+            })
+    );
+
+});
---
+++ src/app/etl-api/hiv-monthly-summary-indicators-resource.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, URLSearchParams } from '@angular/http';
+import { AppSettingsService } from '../app-settings';
+import { DataCacheService } from '../shared/services/data-cache.service';
+@Injectable()
+export class HivMonthlySummaryIndicatorsResourceService {
+    constructor(protected http: Http, protected appSettingsService: AppSettingsService,
+                private cacheService: DataCacheService) { }
+
+    public getUrl(): string {
+        return this.appSettingsService.getEtlRestbaseurl().trim() + `hiv-monthly-summary-indicators`;
+    }
+
+    public getPatientListUrl(): string {
+        return this.appSettingsService.getEtlRestbaseurl().trim()
+            + `hiv-monthly-summary-indicators/patient-list`;
+    }
+
+    public getUrlRequestParams(params): URLSearchParams {
+        let urlParams: URLSearchParams = new URLSearchParams();
+
+        if (params.indicators) {
+            urlParams.set('indicators', params.indicators);
+        }
+
+        if (params.indicator) {
+            urlParams.set('indicator', params.indicator);
+        }
+
+        urlParams.set('endDate', params.endDate);
+        urlParams.set('gender', params.gender);
+        urlParams.set('startDate', params.startDate);
+        urlParams.set('locationUuids', params.locationUuids);
+        urlParams.set('startAge', params.startAge);
+        urlParams.set('endAge', params.endAge);
+
+        return urlParams;
+    }
+
+    public getHivSummaryMonthlyIndicatorsReport(params) {
+        let urlParams = this.getUrlRequestParams(params);
+        let url = this.getUrl();
+        let request = this.http.get(url, {
+            search: urlParams
+        })
+            .map((response: Response) => {
+                return response.json();
+            });
+
+        return this.cacheService.cacheRequest(url, urlParams, request);
+
+    }
+
+    public getHivSummaryMonthlyIndicatorsPatientList(params) {
+        let urlParams = this.getUrlRequestParams(params);
+        if (!params.startIndex) {
+            params.startIndex = '0';
+        }
+        if (!params.limit) {
+            params.limit = '300';
+        }
+        urlParams.set('startIndex', params.startIndex);
+        urlParams.set('limit', params.limit);
+        let url = this.getPatientListUrl();
+        let request = this.http.get(url, {
+            search: urlParams
+        })
+            .map((response: Response) => {
+                return response.json().result;
+            });
+
+        this.cacheService.cacheRequest(url, urlParams, request);
+        return request;
+    }
+}
