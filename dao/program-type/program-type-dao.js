eb97d361265bb5f9bae22773a906196b3c0adb3f
felix <maikofelix89@gmail.com>
2017-12-11 11:31:23 +0300

    ERS-214:Clinic Dashboard Filter Fix

---
+++ dao/program-type/program-type-dao.js
@@ -0,0 +1,50 @@
+/*jshint -W003, -W097, -W117, -W026 */
+'use strict';
+
+var Promise = require('bluebird');
+var squel = require('squel');
+var _ = require('underscore');
+var moment = require('moment');
+var connection = require('../../dao/connection/mysql-connection-service.js');
+var authorizer = require('../../authorization/etl-authorizer');
+
+var programType = {
+    getProgramTypes : getProgramTypes
+};
+
+module.exports = programType;
+
+function getProgramTypes() {
+
+    return new Promise(function (resolve, reject) {
+            connection.getServerConnection()
+            .then(function (conn) {
+                   var query = squel.select()
+                    .field('p.program_id')
+                    .field('p.name')
+                    .field('p.uuid')
+                    .from('amrs.program', 'p')
+                    .toString();
+                conn.query(query, {}, function (err, rows, fields) {
+                    if (err) {
+                        reject('Error querying server');
+                    }
+                    else {
+                        resolve(rows);
+                    }
+                    conn.release();
+                });
+            })
+            .catch(function (err) {
+                reject('Error establishing connection to MySql Server');
+            });
+
+
+
+    });
+   
+}
+
+
+
+
