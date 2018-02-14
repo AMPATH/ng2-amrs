4f497258dee7c5bcb3e97f1b6e59e720e0ed32b3
Nicky Kibor <nickykibor@Nickys-MacBook-Pro.local>
2018-02-14 17:48:52 +0300

    Fixed voiding script

--- worker/void-conflicting-obs-script.js
+++ worker/void-conflicting-obs-script.js
@@ -34,6 +34,7 @@ var s = fs.createReadStream(input_file)
             if (line && line !== '') {
                 console.log('voiding obs: ', line);
                 var openmrsAppName = config.openmrs.applicationName || 'amrs';
+                var protocol = config.etl.tls ? 'https' : 'http';
                 var url = protocol + '://' + config.openmrs.host + ':' + config.openmrs.port + '/' + openmrsAppName + '/ws/rest/v1/obs/' + line + '?!purge';
 
                 var usernamePass = config.eidSyncCredentials.username + ":" + config.eidSyncCredentials.password;
