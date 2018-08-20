d49e352820f272e4afd9d541337f0e7c5591efbf
kwatuha <kwatuha@gmail.com>
2018-08-20 12:05:34 +0300

    HIV-110:Fix patient identifier editing issue

--- src/app/patient-creation/patient-creation.service.ts
+++ src/app/patient-creation/patient-creation.service.ts
@@ -6,6 +6,7 @@ import {
     PatientCreationResourceService
 } from '../openmrs-api/patient-creation-resource.service';
 import { Patient } from '../models/patient.model';
+import * as _ from 'lodash';
 
 @Injectable()
 export class PatientCreationService {
@@ -74,12 +75,13 @@ export class PatientCreationService {
         }
         sum = Math.abs(sum) + 10;
         let digit = (10 - (sum % 10)) % 10;
+        console.log('Required check digit', digit);
         return digit;
 
     }
 
     public checkRegexValidity(expression, identifier) {
-        let identifierRegex = new RegExp('^' + expression + '$');
+        let identifierRegex = new RegExp(expression);
         return (identifierRegex.test(identifier));
     }
 
@@ -90,8 +92,72 @@ export class PatientCreationService {
     public commonIdentifierTypes() {
         return [
             'KENYAN NATIONAL ID NUMBER',
+            'AMRS Universal ID',
             'AMRS Medical Record Number',
             'CCC Number'
         ];
     }
+
+    public getIdentifierTypeFormat(identifierType) {
+        let formatRestrictedIdentifiers = this.identifierTypeFormat();
+        return _.filter(formatRestrictedIdentifiers, (identifierTypeFormat) => {
+          if (identifierTypeFormat.val === identifierType) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      }
+        private identifierTypeFormat() {
+          return [
+              {label: 'AMRS Medical Record Number', format: null, checkdigit: 1,
+              val: '58a46e2e-1359-11df-a1f1-0026b9348838'},
+              {label: 'AMRS Universal ID', format: null, checkdigit: 1,
+              val: '58a4732e-1359-11df-a1f1-0026b9348838'},
+              {label: 'CCC Number', format: '^\\d{5}-\\d{5}$', checkdigit: null,
+              val: 'f2d6ff1a-8440-4d35-a150-1d4b5a930c5e'},
+              {label: 'AMPATH Staff PF Number', format: '^AM-\\d{4,5}$', checkdigit: null,
+              val: 'c700c468-57b4-45c5-8912-318770c8856d'},
+              {label: 'Anticoagulation Clinic Number', format: '^AC-\\d{4,5}$', checkdigit: null,
+              val: 'c456a1db-1c7c-4b82-be09-867d9a3abb61'},
+              {label: 'HCT ID', format: null, checkdigit: 1,
+              val: '58a4723e-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH Amenity Number', format: '^AOPD-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a4809e-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH ANC Number', format: '^ANC-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a4844a-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH Casualty Number', format: '^CAS-\\d{2,5}/\\d{2}$', checkdigit: null,
+              val: '58a47608-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH CWC Number', format: '^CWC-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a48530-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH Dental Number', format: '^DC-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a48274-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH ENT Number', format: '^ENT-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a4835a-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH Eye Clinic Number', format: '^ER-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a488d2-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH FP Number', format: '^FP-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a47cf2-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH IP Number', format: '^IP-\\d{3,7}$', checkdigit: null,
+              val: '58a47ec8-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH Memorial Number', format: '^MW-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a48184-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH OPD Number', format: '^OPD-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a47bf8-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH ORTHO Number', format: '^OT-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a48616-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH SCC Number', format: '^SCC-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a47de2-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH Staff PF Number', format: '^PF-\\d{4,5}$', checkdigit: null,
+              val: '58a487ec-1359-11df-a1f1-0026b9348838'},
+              {label: 'MTRH X-Ray Number', format: '^X-\\d{3,5}/\\d{2}$', checkdigit: null,
+              val: '58a47fae-1359-11df-a1f1-0026b9348838'},
+              {label: 'OVCID', format: '^\\d{8}-\\d{14}$', checkdigit: null,
+              val: '52b78065-5c0f-4cf8-ab02-6b1b992b18c4'},
+              {label: 'pMTCT ID', format: null, checkdigit: 1,
+              val: '58a47144-1359-11df-a1f1-0026b9348838'},
+              {label: 'X Number', format: null, checkdigit: 1,
+              val: '58a4741e-1359-11df-a1f1-0026b9348838'}
+          ];
+      }
 }
