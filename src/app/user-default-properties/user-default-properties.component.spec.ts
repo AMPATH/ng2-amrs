3fe3777a7e1a5834db611ec7e3ba2f54ab60e56e
Rodgers Tanui <rtankip@gmail.com>
2019-01-03 16:25:30 +0300

    User Default Settings Tests and Lint

--- src/app/user-default-properties/user-default-properties.component.spec.ts
+++ src/app/user-default-properties/user-default-properties.component.spec.ts
@@ -1,43 +1,65 @@
-import { MockBackend } from '@angular/http/testing';
-import { Http, BaseRequestOptions } from '@angular/http';
+/* tslint:disable:no-inferrable-types */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, ComponentFixture, inject } from '@angular/core/testing';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, convertToParamMap, ParamMap } from '@angular/router';
 import { Location, APP_BASE_HREF } from '@angular/common';
 import { SpyLocation } from '@angular/common/testing';
-
 import { AppSettingsService } from '../app-settings/app-settings.service';
 import { LocalStorageService } from '../utils/local-storage.service';
 import { UserDefaultPropertiesComponent } from './user-default-properties.component';
 import { UserDefaultPropertiesService } from './user-default-properties.service';
 import { UserService } from '../openmrs-api/user.service';
-import { User } from '../models/user.model';
 import { SessionStorageService } from '../utils/session-storage.service';
 import { UserMockService } from './user.service.mock';
 import { UserDefaultPropertiesModule } from './user-default-properties.module';
 import { BehaviorSubject } from 'rxjs';
 import { DataCacheService } from '../shared/services/data-cache.service';
 import { CacheModule, CacheService } from 'ionic-cache';
-import { FakeRetrospectiveDataEntryService
+import { CacheStorageService } from 'ionic-cache/dist/cache-storage';
+import { IonicStorageModule } from '@ionic/storage';
+import {
+  FakeRetrospectiveDataEntryService
 } from '../retrospective-data-entry/services/retrospective-data-entry-mock.service';
-import { RetrospectiveDataEntryService
+import {
+  RetrospectiveDataEntryService
 } from '../retrospective-data-entry/services/retrospective-data-entry.service';
 import { ProviderResourceService } from '../openmrs-api/provider-resource.service';
 import { Observable } from 'rxjs';
 import { ReplaySubject } from 'rxjs';
+import 'rxjs/add/observable/of';
 
 class MockActivatedRoute {
-  params;
-  private paramsSubject = new BehaviorSubject(this.testParams);
-  private _params: {};
-  constructor() {
-    this.params = this.paramsSubject.asObservable();
+  // Observable that contains a map of the parameters
+  private subjectParamMap = new BehaviorSubject(convertToParamMap(this.testParamMap));
+  paramMap = this.subjectParamMap.asObservable();
+
+  private _testParamMap: ParamMap;
+  get testParamMap() {
+    return this._testParamMap;
   }
-  get testParams() {
-    return this._params;
+  set testParamMap(params: {}) {
+    this._testParamMap = convertToParamMap(params);
+    this.subjectParamMap.next(this._testParamMap);
   }
-  set testParams(newParams: any) {
-    this._params = newParams;
-    this.paramsSubject.next(newParams);
+
+  // Observable that contains a map of the query parameters
+  private subjectQueryParamMap = new BehaviorSubject(convertToParamMap(this.testParamMap));
+  queryParamMap = this.subjectQueryParamMap.asObservable();
+
+  private _testQueryParamMap: ParamMap;
+  get testQueryParamMap() {
+    return this._testQueryParamMap;
+  }
+  set testQueryParamMap(params: {}) {
+    this._testQueryParamMap = convertToParamMap(params);
+    this.subjectQueryParamMap.next(this._testQueryParamMap);
+  }
+
+  get snapshot() {
+    return {
+      paramMap: this.testParamMap,
+      queryParamMap: this.testQueryParamMap
+    };
   }
 }
 
@@ -51,7 +73,7 @@ class MockPropertyService {
   }
 
   getUserProperty() {
-    return {}
+    return {};
   }
 }
 
@@ -63,17 +85,22 @@ class MockProviderResourceService {
   }
 
   public searchProvider(searchText: string, cached: boolean = false, v: string = null):
-  Observable<any> {
+    Observable<any> {
     return Observable.of({});
   }
 
   public getProviderByUuid(uuid: string, cached: boolean = false, v: string = null):
-  Observable<any> {
-    return Observable.of({})
+    Observable<any> {
+    return Observable.of({});
   }
   public getProviderByPersonUuid(uuid) {
-    let providerResults = new ReplaySubject(1);
+    const providerResults = new ReplaySubject(1);
     return providerResults;
+  }
+}
+
+class StorageService {
+  constructor(a, b) {
   }
 }
 
@@ -88,43 +115,41 @@ describe('Component: User Default Settings Unit Tests', () => {
   beforeEach(async(() => {
     activeRoute = new MockActivatedRoute();
     TestBed.configureTestingModule({
-      imports: [UserDefaultPropertiesModule],
+      imports: [UserDefaultPropertiesModule, HttpClientTestingModule, CacheModule, IonicStorageModule],
       providers: [
-        MockBackend,
-        BaseRequestOptions,
-        {provide: APP_BASE_HREF, useValue: '/'},
-        {
-          provide: Http,
-          useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
-            return new Http(backendInstance, defaultOptions);
-          },
-          deps: [MockBackend, BaseRequestOptions]
-        },
+        { provide: APP_BASE_HREF, useValue: '/' },
         {
           provide: UserDefaultPropertiesService,
           useClass: MockPropertyService
         },
         {
           provide: RetrospectiveDataEntryService, useFactory: () => {
-          return new FakeRetrospectiveDataEntryService();
-        }
+            return new FakeRetrospectiveDataEntryService();
+          }
         },
-        { provide : Location, useClass: SpyLocation },
-        { provide : Router, useValue : jasmine.createSpyObj('Router', ['navigate'])  },
+        { provide: Location, useClass: SpyLocation },
+        { provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate']) },
         AppSettingsService,
         {
           provide: UserService,
           useValue: new UserMockService(null)
         },
-        { provide: ActivatedRoute, useClass: MockActivatedRoute },
+        {
+          provide: ActivatedRoute, useClass: MockActivatedRoute
+        },
         SessionStorageService,
         LocalStorageService,
         DataCacheService,
         CacheService,
         {
+          provide: CacheStorageService, useFactory: () => {
+            return new StorageService(null, null);
+          }, deps: []
+        },
+        {
           provide: ProviderResourceService, useFactory: () => {
-          return new MockProviderResourceService();
-        }
+            return new MockProviderResourceService();
+          }
         },
         LocalStorageService
       ]
@@ -135,6 +160,8 @@ describe('Component: User Default Settings Unit Tests', () => {
         component = fixture.componentInstance;
         injector = fixture.debugElement.injector;
         propertiesResourceService = TestBed.get(UserDefaultPropertiesService);
+        activeRoute = fixture.debugElement.injector.get(ActivatedRoute) as any;
+        activeRoute.testParamMap = { confirm: 1 };
       });
 
   }));
@@ -143,71 +170,14 @@ describe('Component: User Default Settings Unit Tests', () => {
     TestBed.resetTestingModule();
   });
 
-  xit('should instantiate the component', (done) => {
+  it('should instantiate the component', (done) => {
 
     expect(component).toBeTruthy();
     done();
 
   });
 
-  // it('should set the default value of location from route params if you they are defined',
-  //   inject([Router, ActivatedRoute, UserDefaultPropertiesService, UserService],
-  //     (r: Router, route: ActivatedRoute, t: UserDefaultPropertiesService, u: UserService) => {
-  //       activeRoute.testParams = { confirm: 1 };
-  //       component.ngOnInit();
-  //       fixture.detectChanges();
-  //       route.params.subscribe((params) => {
-  //         expect(params['confirm']).toEqual(1);
-  //         expect(component.query).toEqual('test location');
-  //       });
-  // }));
-
-  // it('should have required properties', (done) => {
-  //   expect(component.locations.length).toEqual(0);
-  //   expect(component.isBusy).toBeFalsy();
-  //   expect(typeof component.user).toBeDefined(User);
-  //   expect(component.query).toEqual('');
-  //   expect(component.filteredList.length).toEqual(0);
-  //   expect(component.currentLocation).toEqual('');
-  //   expect(component.selectedIdx).toEqual(-1);
-  //   done();
-
-  // });
-
-  // beforeEach((done) => {
-
-  //   spyOn(component, 'ngOnInit').and.callFake(() => {});
-  //   component.ngOnInit();
-  //   expect(component.ngOnInit).toHaveBeenCalled();
-
-  //   spyOn(component, 'goToPatientSearch').and.callFake(() => {});
-  //   component.goToPatientSearch();
-  //   expect(component.goToPatientSearch).toHaveBeenCalled();
-
-  //   spyOn(component, 'filter').and.callFake(() => {});
-  //   component.filter('click');
-  //   expect(component.filter).toHaveBeenCalled();
-
-  //   spyOn(component, 'select').and.callFake(() => {});
-  //   component.select({display: 'test'});
-  //   expect(component.select).toHaveBeenCalled();
-
-  //   done();
-
-  // });
-  xit('should set default value of location from route params if they are defined',
-    inject([Router, ActivatedRoute, UserDefaultPropertiesService, UserService],
-      (r: Router, route: ActivatedRoute, t: UserDefaultPropertiesService, u: UserService) => {
-        activeRoute.testParams = { confirm: 1 };
-        component.ngOnInit();
-        fixture.detectChanges();
-        route.params.subscribe((params) => {
-          expect(params['confirm']).toEqual(1);
-          expect(component.currentLocation).toEqual('test location');
-        });
-  }));
-
-  xit('should have required properties', (done) => {
+  it('should have required properties', (done) => {
     expect(component.locations.length).toEqual(0);
     expect(component.isBusy).toBeFalsy();
     expect(component.currentLocation).toBeFalsy(undefined);
@@ -215,22 +185,18 @@ describe('Component: User Default Settings Unit Tests', () => {
 
   });
 
-  xit('should have all the required functions defined and callable', (done) => {
+  it('should have all the required functions defined and callable', (done) => {
 
-    spyOn(component, 'ngOnInit').and.callFake(() => {});
+    spyOn(component, 'ngOnInit').and.callFake(() => { });
     component.ngOnInit();
     expect(component.ngOnInit).toHaveBeenCalled();
 
-    spyOn(component, 'goToPatientSearch').and.callFake(() => {});
+    spyOn(component, 'goToPatientSearch').and.callFake(() => { });
     component.goToPatientSearch();
     expect(component.goToPatientSearch).toHaveBeenCalled();
 
-    /*spyOn(component, 'filter').and.callFake(() => {});
-    component.filter('click');
-    expect(component.filter).toHaveBeenCalled();*/
-
-    spyOn(component, 'select').and.callFake(() => {});
-    component.select({display: 'test'});
+    spyOn(component, 'select').and.callFake(() => { });
+    component.select({ display: 'test' });
     expect(component.select).toHaveBeenCalled();
 
     done();
--- src/app/user-default-properties/user-default-properties.service.ts
+++ src/app/user-default-properties/user-default-properties.service.ts
@@ -12,14 +12,15 @@ export class UserDefaultPropertiesService {
   public locationSubject = new BehaviorSubject<any>('');
   private user: User;
 
-  constructor(private userService: UserService
-    ,         private localStorage: LocalStorageService
-    ,         private http: HttpClient
-    ,         private appSettingsService: AppSettingsService) { }
+  constructor(
+    private userService: UserService,
+    private localStorage: LocalStorageService,
+    private http: HttpClient,
+    private appSettingsService: AppSettingsService) { }
 
   public getLocations(): Observable<any> {
 
-    let api = this.appSettingsService.getOpenmrsServer() + '/ws/rest/v1/location?v=default';
+    const api = this.appSettingsService.getOpenmrsServer() + '/ws/rest/v1/location?v=default';
 
     return this.http.get(api);
 
@@ -27,14 +28,14 @@ export class UserDefaultPropertiesService {
 
   public getCurrentUserDefaultLocation() {
 
-    let userDisplay = this.getAuthenticatedUser() ?  this.getAuthenticatedUser().display : null;
-    let location = this.localStorage.getItem('userDefaultLocation' + userDisplay);
+    const userDisplay = this.getAuthenticatedUser() ? this.getAuthenticatedUser().display : null;
+    const location = this.localStorage.getItem('userDefaultLocation' + userDisplay);
     return JSON.parse(location) ? JSON.parse(location).display : undefined;
   }
 
   public getCurrentUserDefaultLocationObject() {
-    let userDisplay = this.getAuthenticatedUser() ? this.getAuthenticatedUser().display : null;
-    let location = this.localStorage.getItem('userDefaultLocation' + userDisplay);
+    const userDisplay = this.getAuthenticatedUser() ? this.getAuthenticatedUser().display : null;
+    const location = this.localStorage.getItem('userDefaultLocation' + userDisplay);
     if (location) {
       return JSON.parse(location);
     }
