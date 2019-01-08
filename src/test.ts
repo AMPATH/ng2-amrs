29a732e2572031899490aca2ff67db76e813ad26
madvincy <eugenekandie@gmail.com>
2019-01-08 13:42:22 +0300

    report-utilities test fixes

--- src/test.ts
+++ src/test.ts
@@ -15,7 +15,8 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /app\/patient-dashboard\/common\/formentry\/form-submission\.service\.spec\.ts$/);
+
+const context = require.context('./', true, /app\/reporting-utilities\/report-filter\/report-filter\.component\.spec\.ts$/);
 // const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
