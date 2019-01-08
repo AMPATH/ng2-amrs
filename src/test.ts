d23dc08fc9ee427f637aa93b25f14191bcd265db
madvincy <eugenekandie@gmail.com>
2019-01-08 16:10:57 +0300

    utils test fixes

--- src/test.ts
+++ src/test.ts
@@ -15,7 +15,9 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /app\/patient-dashboard\/common\/formentry\/form-submission\.service\.spec\.ts$/);
+// const context = require.context('./', true, /app\/patient-dashboard\/common\/formentry\/form-submission\.service\.spec\.ts$/);
+// const context = require.context('./', true, /app\/utils\/local-storage\.service\.spec\.ts$/);
+const context = require.context('./', true, /app\/utils\/session-storage\.service\.spec\.ts$/);
 // const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
