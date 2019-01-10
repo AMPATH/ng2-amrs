f47ccfbe79520c52741ba4324a0e1d6c5ab3f5b7
madvincy <eugenekandie@gmail.com>
2019-01-10 12:24:49 +0300

    utils test and lint fixes

--- src/test.ts
+++ src/test.ts
@@ -12,8 +12,8 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require
-.context('./', true, /app\/patient-dashboard\/common\/visit\/visit-starter\/visit-starter\.component\.spec.ts$/);
+const context = require.context('./', true, /app\/utils\/local-storage\.service\.spec\.ts$/);
+// const context = require.context('./', true, /app\/utils\/session-storage\.service\.spec\.ts$/);
 // const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
