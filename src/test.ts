a1aa89688f9493902af9d27333e1270ad68af327
jecihjoy <jecihjoy@gmail.com>
2019-01-10 10:10:46 +0300

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/zone-testing';
-import {getTestBed} from '@angular/core/testing';
-import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
 
@@ -12,9 +15,9 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-// And load the modules.
-context.keys().map(context);
-const context = require.context('./', true, /app\/online-tracker\/online-tracker\.service\.spec\.ts$/);
+const context = require
+.context('./', true, /app\/etl-api\/user-cohort-resource\.service\.spec.ts$/);
+// const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 
