3b3ca61e605f360a48f7344ea840c01e2699e7a8
Emmanuel Nyachoke <achachiez@gmail.com>
2019-01-11 17:02:20 +0300

    Add karma parallel

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,15 +4,20 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-mocha-reporter'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-parallel')
     ],
+    parallelOptions: {
+      executors: (Math.ceil(require('os').cpus().length)),
+      shardStrategy: 'round-robin'
+    },
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       jasmine: {
