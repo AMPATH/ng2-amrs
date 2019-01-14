97a116cf68deb42aca1767c9a122667db76e9687
Ben Ronnie <webtrendz@live.com>
2019-01-14 11:16:13 +0300

    HIV Triage Vitals Fix

--- src/app/patient-dashboard/common/todays-vitals/todays-vitals.service.ts
+++ src/app/patient-dashboard/common/todays-vitals/todays-vitals.service.ts
@@ -15,7 +15,7 @@ import { VitalsDatasource } from './vitals.datasource';
 @Injectable()
 export class TodaysVitalsService {
   public patient: Patient = new Patient({});
-  private dataSources: any[] = [];
+  private vitalSources: any[] = [];
 
   constructor(
     private visitResourceService: VisitResourceService,
@@ -24,19 +24,19 @@ export class TodaysVitalsService {
 
   public getTodaysVitals(patient: Patient, todaysEncounters, sources) {
     this.patient = patient;
-    this.dataSources = sources || [];
+    this.vitalSources = sources || [];
     return new Promise((resolve, reject) => {
       for (let encounterItem of todaysEncounters) {
         this.getVitalsFromObs(encounterItem.obs);
       }
-      resolve(this.vitalsDataSource.dataSources);
+      resolve(this.vitalsDataSource.vitalSources);
     });
 
   }
 
   private getVitalsFromObs(obsArray) {
     let createdVital: any;
-    _.each(this.dataSources, (source) => {
+    _.each(this.vitalSources, (source) => {
       const vitalSource = new source(new Vital({}), this.patient);
       for (let obs of obsArray) {
         let ob = obs;
@@ -45,22 +45,22 @@ export class TodaysVitalsService {
           if (typeof createdVital !== 'undefined' && createdVital.name) {
             const vitalModel = createdVital[createdVital.name];
             if (vitalModel && vitalModel.show) {
-              this.vitalsDataSource.addToSource(vitalModel);
+              this.vitalsDataSource.addToVitalSource(vitalModel);
             }
           }
         }
       }
-      if(!this.vitalsDataSource.hasVital('bmi')) {
-        this.vitalsDataSource.addToSource(vitalSource.getBMI(createdVital));
+      if (!this.vitalsDataSource.hasVital('bmi')) {
+        this.vitalsDataSource.addToVitalSource(vitalSource.getBMI(createdVital));
       }
     });
     this.applyCompounding();
   }
 
   private applyCompounding() {
-    let compounds = _.filter(this.vitalsDataSource.dataSources, 'isCompoundedWith');
+    let compounds = _.filter(this.vitalsDataSource.vitalSources, 'isCompoundedWith');
     _.each(compounds, (compound) => {
-      let toCompound = _.find(this.vitalsDataSource.dataSources, (s) => compound.isCompoundedWith === s.name);
+      let toCompound = _.find(this.vitalsDataSource.vitalSources, (s) => s && compound.isCompoundedWith === s.name);
       if (toCompound) {
         toCompound.compoundValue = compound;
       }
