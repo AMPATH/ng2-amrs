5a42b9f86bf476861421ac6c4ffea409db6e106c
Emmanuel Nyachoke <achachiez@gmail.com>
2019-01-17 14:36:13 +0300

    Save encrypted session response

--- src/app/openmrs-api/authentication.service.ts
+++ src/app/openmrs-api/authentication.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
-import {take} from 'rxjs/operators';
+import { take } from 'rxjs/operators';
+
+import * as PouchDB from 'pouchdb/dist/pouchdb';
+PouchDB.plugin(require('pouchdb-upsert'));
+
 import { AppSettingsService } from '../app-settings/app-settings.service';
 import { SessionService } from './session.service';
 import { LocalStorageService } from '../utils/local-storage.service';
 import { SessionStorageService } from '../utils/session-storage.service';
 import { Constants } from '../utils/constants';
 import { CookieService } from 'ngx-cookie';
+import { AesHelper } from './aes-helper';
 
 @Injectable()
 export class AuthenticationService {
-
+  private _pouchDB: any;
   constructor(
     private appSettingsService: AppSettingsService,
     private localStorageService: LocalStorageService,
     private sessionStorageService: SessionStorageService,
     private sessionService: SessionService,
-    private _cookieService: CookieService) { }
+    private _cookieService: CookieService) {
+    this._pouchDB = new PouchDB('users');
+  }
 
   public authenticate(username: string, password: string) {
 
@@ -28,19 +35,32 @@ export class AuthenticationService {
 
     request
       .pipe(take(1)).subscribe(
-      (response: any) => {
+        (response: any) => {
 
-        let data = response;
+          let data = response;
 
-        if (data.authenticated) {
+          if (data.authenticated) {
 
-          this.setCredentials(username, password);
+            this.setCredentials(username, password);
 
-          // store logged in user details in session storage
-          let user = data.user;
-          this.storeUser(user);
-        }
-      });
+            // store logged in user details in session storage
+            let helper = new AesHelper();
+            helper.encrypt(password, JSON.stringify(data))
+              .then(v => {
+                console.log("ENCRYPTED", v)
+                this._pouchDB.upsert(username, function (doc) {
+                  return {
+                    userdata: v,
+                    dateTime: new Date()
+                  }
+                });
+
+              });
+
+            let user = data.user;
+            this.storeUser(user);
+          }
+        });
 
     return request;
   }
@@ -51,14 +71,14 @@ export class AuthenticationService {
 
     response.pipe(
       take(1)).subscribe(
-      (res: Response) => {
+        (res: Response) => {
 
-        this.clearSessionCache();
-      },
-      (error: Error) => {
+          this.clearSessionCache();
+        },
+        (error: Error) => {
 
-        this.clearSessionCache();
-      });
+          this.clearSessionCache();
+        });
 
     return response;
   }
@@ -71,9 +91,9 @@ export class AuthenticationService {
   // This will clear motd alert cookies set  at every log in
   public clearLoginAlertCookies() {
 
-      let cookieKey = 'motdLoginCookie';
+    let cookieKey = 'motdLoginCookie';
 
-      this._cookieService.remove(cookieKey);
+    this._cookieService.remove(cookieKey);
 
   }
 
