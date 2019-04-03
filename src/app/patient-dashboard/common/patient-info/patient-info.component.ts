3425b9d157a6286c3fd731f78e756f4dcb0f07ae
maikofelix89 <maikofelix89@gmail.com>
2019-04-03 15:40:28 +0300

    MCH-34:Request to put relationships at the patient header

--- src/app/patient-dashboard/common/patient-info/patient-info.component.ts
+++ src/app/patient-dashboard/common/patient-info/patient-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy , AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AppFeatureAnalytics } from '../../../shared/app-analytics/app-feature-analytics.service';
 import { PatientService } from '../../services/patient.service';
 
@@ -10,12 +11,14 @@ import { Subscription } from 'rxjs';
   templateUrl: './patient-info.component.html',
   styleUrls: ['./patient-info.component.css']
 })
-export class PatientInfoComponent implements OnInit, OnDestroy {
+export class PatientInfoComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public patient: Patient;
   public subscription: Subscription;
+  public routeSub: Subscription;
+  public scrollSection = '';
   constructor(private appFeatureAnalytics: AppFeatureAnalytics,
-    private patientService: PatientService) {
+    private patientService: PatientService, private route: ActivatedRoute) {
   }
   public ngOnInit() {
     this.subscription = this.patientService.currentlyLoadedPatient.subscribe(
@@ -28,12 +31,40 @@ export class PatientInfoComponent implements OnInit, OnDestroy {
     );
     this.appFeatureAnalytics
       .trackEvent('Patient Dashboard', 'Patient Info Loaded', 'ngOnInit');
+    this.routeSub = this.route.queryParams
+    .subscribe((params: any) => {
+      if (params.scrollSection) {
+          this.scrollSection = params.scrollSection;
+      }
+    });
   }
 
   public ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
+    if (this.routeSub) {
+          this.routeSub.unsubscribe();
+    }
+  }
+
+  public ngAfterViewInit() {
+    if (this.scrollSection !== '' || typeof this.scrollSection !== 'undefined') {
+      setTimeout(() => {
+        this.scrollToSection(this.scrollSection);
+      }, 3000);
+
+    }
+  }
+
+  public scrollToSection(section: string) {
+        console.log('scroll section', section);
+        const element = document.getElementById(section);
+        console.log('scroll element', element);
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center'
+        });
   }
 
 }
