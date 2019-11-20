5e358d1826487e1a5a0648bf19ca882dc2b23182
Dennis Kigen <kigen.work@gmail.com>
2019-11-20 14:26:07 +0300

    POCONC-159: Fixes and improvements to the oncology snapshot display

--- src/app/patient-dashboard/oncology/program-snapshot/oncology-program-snapshot.component.ts
+++ src/app/patient-dashboard/oncology/program-snapshot/oncology-program-snapshot.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 import * as _ from 'lodash';
+
 import { Patient } from '../../../models/patient.model';
-import { OncologySummaryResourceService
+import {
+  OncologySummaryResourceService
 } from '../../../etl-api/oncology-summary-resource.service';
 
 @Component({
@@ -15,14 +19,16 @@ export class OncologyProgramSnapshotComponent implements OnInit, OnDestroy {
   @Input() public programUuid;
   public loadingSummary = false;
   public hasData = false;
+  public hasLoadedData = false;
   public isIntegratedProgram = false;
   public hasError = false;
   public subscription: Subscription;
   public patientUuid: any;
   public errors: any = [];
   public summaryData: any;
+  public oncologyScreeningAndDiagnosisProgramUuid = '37ff4124-91fd-49e6-8261-057ccfb4fcd0';
   constructor(
-    private oncolologySummary: OncologySummaryResourceService,
+    private oncologySummary: OncologySummaryResourceService,
     private integratedProgramSnapshot: OncologySummaryResourceService) {
   }
 
@@ -32,7 +38,7 @@ export class OncologyProgramSnapshotComponent implements OnInit, OnDestroy {
         this.hasError = true;
       } else {
         this.hasData = false;
-        if (this.programUuid === '37ff4124-91fd-49e6-8261-057ccfb4fcd0') {
+        if (this.programUuid === this.oncologyScreeningAndDiagnosisProgramUuid) {
           this.loadScreeningAndDiagnosisData(patientUuid);
         } else {
           this.loadOncologyDataSummary(patientUuid);
@@ -50,16 +56,20 @@ export class OncologyProgramSnapshotComponent implements OnInit, OnDestroy {
 
   public loadOncologyDataSummary(patientUuid) {
     if (this.programUuid && patientUuid) {
-      this.oncolologySummary.getOncologySummary('summary', patientUuid, this.programUuid).subscribe((summary) => {
-        this.summaryData = summary[0];
-        this.hasData =  true;
-        this.hasError = false;
+      this.loadingSummary = true;
+      this.oncologySummary.getOncologySummary('summary', patientUuid, this.programUuid).pipe(take(1)).subscribe((summary) => {
+        this.hasLoadedData = true;
         this.loadingSummary = false;
+        if (summary.length) {
+          this.summaryData = summary[0];
+          this.hasData = true;
+          this.hasError = false;
+        }
       }, (error) => {
         this.loadingSummary = false;
         this.hasData = false;
         this.hasError = true;
-        console.log(error);
+        console.error('Error fetching oncology summary: ', error);
         this.errors.push({
           id: 'summary',
           message: 'Error Fetching Summary'
@@ -69,20 +79,23 @@ export class OncologyProgramSnapshotComponent implements OnInit, OnDestroy {
   }
 
   private loadScreeningAndDiagnosisData(patientUuid: any) {
-    this.hasData =  true;
+    this.hasData = false;
     this.hasError = false;
     this.integratedProgramSnapshot.getIntegratedProgramSnapshot(patientUuid)
       .subscribe((screeningSummary) => {
-        this.isIntegratedProgram = true;
-        this.summaryData = screeningSummary;
-        this.hasData =  true;
-        this.hasError = false;
         this.loadingSummary = false;
+        this.hasLoadedData = true;
+        if (screeningSummary.length) {
+          this.summaryData = screeningSummary;
+          this.isIntegratedProgram = true;
+          this.hasData = true;
+          this.hasError = false;
+        }
       }, (error => {
         this.loadingSummary = false;
         this.hasData = false;
         this.hasError = true;
-        console.log(error);
+        console.error('Error fetching integrated screening summary: ', error);
         this.errors.push({
           id: 'summary',
           message: 'Error Fetching Summary'
