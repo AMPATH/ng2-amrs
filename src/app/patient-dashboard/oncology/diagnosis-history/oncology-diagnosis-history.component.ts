ad06104abd5f425c594727234ab3c961d6ceec0a
Dennis Kigen <kigen.work@gmail.com>
2019-11-21 08:47:38 +0300

    POCONC-161: Fixes and improvements to oncology diagnosis and medication history

--- src/app/patient-dashboard/oncology/diagnosis-history/oncology-diagnosis-history.component.ts
+++ src/app/patient-dashboard/oncology/diagnosis-history/oncology-diagnosis-history.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Patient } from '../../../models/patient.model';
 import { PatientService } from '../../services/patient.service';
-import { OncologySummaryResourceService
+import {
+  OncologySummaryResourceService
 } from '../../../etl-api/oncology-summary-resource.service';
 
 @Component({
@@ -12,7 +12,8 @@ import { OncologySummaryResourceService
   styles: []
 })
 export class OncologyDiagnosisHistoryComponent implements OnInit, OnDestroy {
-
+  public summaryLoaded = false;
+  public hasData = false;
   public loadingSummary = false;
   public subscription: Subscription;
   public patient: Patient;
@@ -24,7 +25,7 @@ export class OncologyDiagnosisHistoryComponent implements OnInit, OnDestroy {
 
   constructor(
     private patientService: PatientService,
-    private oncolologySummary: OncologySummaryResourceService) {
+    private oncologySummary: OncologySummaryResourceService) {
   }
 
   public ngOnInit() {
@@ -40,32 +41,36 @@ export class OncologyDiagnosisHistoryComponent implements OnInit, OnDestroy {
   public getPatient() {
     this.loadingSummary = true;
     this.subscription = this.patientService.currentlyLoadedPatient.subscribe((patient) => {
-        if (patient) {
-          this.patient = patient;
-          this.patientUuid = this.patient.person.uuid;
-          this.loadOncologyDiagnosisHistory();
-        }
-      }, (err) => {
-        this.loadingSummary = false;
-        this.errors.push({
-          id: 'patient',
-          message: 'error fetching patient'
-        });
+      if (patient) {
+        this.patient = patient;
+        this.patientUuid = this.patient.person.uuid;
+        this.loadOncologyDiagnosisHistory();
+      }
+    }, (err) => {
+      this.loadingSummary = false;
+      this.errors.push({
+        id: 'patient',
+        message: 'error fetching patient'
       });
+    });
   }
 
   public loadOncologyDiagnosisHistory() {
-    this.oncolologySummary.getOncologySummary('diagnosis-history', this.patientUuid, this.programUuid).subscribe((summary) => {
-      this.diagnosisChanges = summary;
-      this.loadingSummary = false;
-    }, (error) => {
-      this.loadingSummary = false;
-      console.log(error);
-      this.errors.push({
-        id: 'summary',
-        message: 'error fetching medication history'
+    this.oncologySummary.getOncologySummary('diagnosis-history', this.patientUuid, this.programUuid)
+      .subscribe((summary) => {
+        this.loadingSummary = false;
+        this.summaryLoaded = true;
+        if (summary.length) {
+          this.diagnosisChanges = summary;
+          this.hasData = true;
+        }
+      }, (error) => {
+        this.loadingSummary = false;
+        this.errors.push({
+          id: 'summary',
+          message: 'Error fetching diagnosis history'
+        });
       });
-    });
   }
 
 }
