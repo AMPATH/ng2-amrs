fa7369499ae2c271a1e175afdad8c66a0e8dc20b
maikofelix47 <maikofelix89@gmail.com>
2020-04-29 13:04:50 +0300

    Added mock patient data to case management resource service

--- src/app/hiv-care-lib/case-management/case-management-patient-list.component.ts
+++ src/app/hiv-care-lib/case-management/case-management-patient-list.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { Subscription } from 'rxjs';
@@ -18,7 +18,9 @@ export class CaseManagementPatientListComponent implements OnInit {
 
   public title = '';
   public patients: any = [];
-  public rowData: any = [];
+  public patientList = [];
+  @Input() public rowData = [];
+
   public params: any;
   public busy: Subscription;
   public gridOptions: GridOptions = {
@@ -39,7 +41,7 @@ export class CaseManagementPatientListComponent implements OnInit {
       }
     }
   };
-  public retentionSummaryColdef: any = [
+  public caseMgmtColdef: any = [
     {
       lockPosition: true,
       headerName: 'No',
@@ -52,14 +54,12 @@ export class CaseManagementPatientListComponent implements OnInit {
     {
       headerName: 'Case Manager',
       field: 'case_manager',
-      width: 400,
-      pinned: 'left'
+      width: 400
     },
     {
       headerName: 'Name',
-      field: 'person_name',
-      width: 200,
-      pinned: 'left'
+      field: 'patient_name',
+      width: 200
     },
     {
       headerName: 'Age',
@@ -73,22 +73,22 @@ export class CaseManagementPatientListComponent implements OnInit {
     },
     {
       headerName: 'Last Followup Date',
-      field: 'phone_number',
+      field: 'last_follow_up_date',
       width: 100
     },
     {
       headerName: 'Days Since Followup',
-      field: 'alternate_phone_number',
+      field: 'days_since_follow_up',
       width: 170
     },
     {
       headerName: 'RTC',
-      field: 'program',
+      field: 'rtc_date',
       width: 250
     },
     {
       headerName: 'Phone RTC',
-      field: 'visit_type',
+      field: 'phone_rtc_date',
       width: 200
     },
     {
@@ -109,7 +109,8 @@ export class CaseManagementPatientListComponent implements OnInit {
     {
       headerName: 'Action',
       field: 'action',
-      width: 100
+      width: 100,
+      pinned: 'right'
     },
     {
       headerName: 'Patient Uuid',
