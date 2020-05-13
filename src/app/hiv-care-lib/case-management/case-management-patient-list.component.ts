2ca0f4674668059e075c16c1c445f7dd6e92d23a
derrick rono <derrickrono@gmail.com>
2020-05-13 12:27:54 +0300

    Fix numbers issue resulting from cached managers request

--- src/app/hiv-care-lib/case-management/case-management-patient-list.component.ts
+++ src/app/hiv-care-lib/case-management/case-management-patient-list.component.ts
@@ -298,6 +298,7 @@ export class CaseManagementPatientListComponent implements OnInit {
     );
   }
   public massAssignCaseManagers(isSubmiting) {
+    this.getCaseManagers();
     this.displayMassAssign = true;
     this.patientList = this.gridOptions.api.getSelectedRows();
     const massAssignPayload = {
@@ -321,6 +322,7 @@ export class CaseManagementPatientListComponent implements OnInit {
     this.displayMassAssign = false;
     this.showSuccessAlert = false;
     this.showErrorAlert = false;
+    this.caseManagers = [];
   }
   public incrementCases(data, element, patientList, user_id, user_name) {
     let assignedCase = 0;
