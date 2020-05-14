d1050e155239c0a7d161d9d71cb115c4c06d2573
derrick rono <derrickrono@gmail.com>
2020-05-14 12:34:33 +0300

    Add consent  to patient info page

--- src/app/patient-dashboard/common/patient-info/telecare/telecare.component.ts
+++ src/app/patient-dashboard/common/patient-info/telecare/telecare.component.ts
@@ -3,6 +3,7 @@ import { ObsResourceService } from 'src/app/openmrs-api/obs-resource.service';
 import { PatientService } from 'src/app/patient-dashboard/services/patient.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'telecare-consent',
@@ -24,23 +25,23 @@ export class TelecareComponent implements OnInit, OnDestroy {
     });
   }
   getClientConsent() {
-    // TODO: Use correct concept uuids
-    this.conceptUuid = ['a8a06fc6-1350-11df-a1f1-0026b9348838', 'a899b35c-1350-11df-a1f1-0026b9348838'];
-    this.subscription = this.obsService.getObsPatientObsByConcept(this.patientUuid, this.conceptUuid).subscribe((data) => {
+    this.conceptUuid = ['9d9ccb6b-73ae-48dd-83f9-12c782ce6685', 'a8a06fc6-1350-11df-a1f1-0026b9348838'];
+    const telecareEncounter = 'df1df2d5-58f4-4e67-9394-c559c680cec6';
+    this.subscription = this.obsService.getObsPatientObsByConcepts(this.patientUuid, this.conceptUuid).subscribe((data) => {
       const results = data['results'];
       if (results.length > 0) {
-      const encDateTime = results[0].encounter.encounterDateTime;
-      this.clientConsent.dateofConcent = encDateTime;
+      const encDateTime = results[0].encounter.encounterDatetime;
+      this.clientConsent.dateofConsent = moment(encDateTime).format('DD-MM-YYYY');
       results.forEach(element => {
-        if (element.encounterDateTime === encDateTime) {
+        if (element.encounter.encounterDatetime === encDateTime) {
           if (this.conceptUuid.includes(element.concept.uuid)) {
             if (element.concept.uuid === this.conceptUuid[0]) {
               this.clientConsent.value = element.value;
-            } else if (element.concept.uuid === this.conceptUuid[1]) {
-              this.clientConsent.comments = element.value;
-              if (element.value) {
+              if (element.value.display === 'YES') {
                 this.consented = true;
               }
+            } else if (element.concept.uuid === this.conceptUuid[1] && element.encounter.uuid === telecareEncounter) {
+              this.clientConsent.comments = element.value;
             }
           }
         }
@@ -52,9 +53,8 @@ export class TelecareComponent implements OnInit, OnDestroy {
     if (this.patientUuid === undefined || this.patientUuid === null) {
       return;
     }
-    // TODO: Redirect to correct form
     this.router.navigate(['/patient-dashboard/patient/' + this.patientUuid +
-      '/general/general/landing-page']);
+      '/general/general/formentry/734b61a1-92a6-497e-a743-17bf3c61796e']);
   }
   ngOnDestroy(): void {
     if (this.subscription) {
