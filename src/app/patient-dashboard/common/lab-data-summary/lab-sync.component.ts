aea9a9225a76c353ab6e5858835fd6f081e0d4fd
Nicky Kibor Kimaina <nkimaina@ampath.or.ke>
2020-09-01 14:36:38 +0300

    Manual lab syncing button (#1291)
    Co-authored-by: corneliouz bett <kbett68@gmail.com>

--- src/app/patient-dashboard/common/lab-data-summary/lab-sync.component.ts
+++ src/app/patient-dashboard/common/lab-data-summary/lab-sync.component.ts
@@ -42,13 +42,13 @@ export class LabSyncComponent implements OnInit, OnDestroy {
     }
   }
 
-  public getNewResults() {
+  public getNewResults(refresh = false) {
     this.fetchingResults = true;
     this.error = undefined;
-    this.getCombinedResult().pipe(take(1)).subscribe((results: any[]) => {
+    this.getCombinedResult(refresh).pipe(take(1)).subscribe((results: any[]) => {
       this.fetchingResults = false;
       // the intention of combining is to have both systems sync. So we take just one result
-      const result = results[1][0];
+      const result = results[0][0];
       if (result.errors && result.errors.length > 0) {
         this.error = result.errors;
       } else {
@@ -60,22 +60,21 @@ export class LabSyncComponent implements OnInit, OnDestroy {
     });
   }
 
-  public getCombinedResult(): Observable<any[]> {
+  public getCombinedResult(refresh = false): Observable<any[]> {
     const startDate = Moment('2006-01-01').startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSSZZ');
     const endDate = Moment().startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSSZZ');
     const batch: Observable<any>[] = [];
     batch.push(this.labsResourceService.getNewPatientLabResults({
       startDate: startDate,
       endDate: endDate,
-      patientUuId: this.patient.person.uuid
-    }));
-    batch.push(this.labsResourceService.getUpgradePatientLabResults({
-      patientUuid: this.patient.person.uuid
+      patientUuId: this.patient.person.uuid,
+      refresh: refresh
     }));
     return combineLatest(batch);
   }
 
   public processResult(results: any) {
+    console.log('Processing Results', results);
 
     const data: any = [];
 
