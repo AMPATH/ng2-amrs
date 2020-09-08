ce5852457556a9de9d9d6017de439df00ec80845
Dennis Kigen <kigen.work@gmail.com>
2020-09-08 23:42:15 +0300

    Format src/app/clinic-schedule-lib

--- src/app/clinic-schedule-lib/daily-schedule/daily-schedule-visits.component.ts
+++ src/app/clinic-schedule-lib/daily-schedule/daily-schedule-visits.component.ts
@@ -4,9 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import * as Moment from 'moment';
 
-import {
-  ClinicDashboardCacheService
-} from '../../clinic-dashboard/services/clinic-dashboard-cache.service';
+import { ClinicDashboardCacheService } from '../../clinic-dashboard/services/clinic-dashboard-cache.service';
 import { DailyScheduleResourceService } from '../../etl-api/daily-scheduled-resource.service';
 import { LocalStorageService } from 'src/app/utils/local-storage.service';
 
@@ -16,35 +14,36 @@ import { LocalStorageService } from 'src/app/utils/local-storage.service';
   styleUrls: ['./daily-schedule.component.css']
 })
 export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
-
   @Input() public selectedDate: any;
   public errors: any[] = [];
   public dailyVisitsPatientList: any[] = [];
   public loadingDailyVisits = false;
-  public dataLoaded  = false;
+  public dataLoaded = false;
   public currentTabLoaded = false;
   public selectedVisitTab: any;
-  public nextStartIndex  = 0;
-  public extraColumns: Array<any> = [{
-    headerName: 'Program',
-    width: 200,
-    field: 'program'
-  }];
+  public nextStartIndex = 0;
+  public extraColumns: Array<any> = [
+    {
+      headerName: 'Program',
+      width: 200,
+      field: 'program'
+    }
+  ];
   public filter: any = {
-     'programType': [],
-     'visitType': [],
-     'encounterType': []
+    programType: [],
+    visitType: [],
+    encounterType: []
   };
   public params: any = {
-    'programType': [],
-    'visitType': [],
-    'encounterType': []
+    programType: [],
+    visitType: [],
+    encounterType: []
   };
   public busyIndicator: any = {
     busy: false,
     message: 'Please wait...' // default message
   };
-  public fetchCount  = 0;
+  public fetchCount = 0;
   @Input() public tab: any;
   @Input() public newList: any;
 
@@ -58,15 +57,17 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
   private _data = new BehaviorSubject<any>([]);
   private selectedClinic: any;
   private subs: Subscription[] = [];
-  constructor(private clinicDashboardCacheService: ClinicDashboardCacheService,
-              private dailyScheduleResource: DailyScheduleResourceService,
-              private localStorageService: LocalStorageService,
-              private route: ActivatedRoute) {
-  }
+  constructor(
+    private clinicDashboardCacheService: ClinicDashboardCacheService,
+    private dailyScheduleResource: DailyScheduleResourceService,
+    private localStorageService: LocalStorageService,
+    private route: ActivatedRoute
+  ) {}
 
   public ngOnInit() {
     this.selectedDate = Moment().format('YYYY-MM-DD');
-    const sub = this.clinicDashboardCacheService.getCurrentClinic()
+    const sub = this.clinicDashboardCacheService
+      .getCurrentClinic()
       .subscribe((location) => {
         this.selectedClinic = location;
         if (this.clinicDashboardCacheService.didLocationChange(location)) {
@@ -85,35 +86,30 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
   }
 
   public loadData() {
-    const routeSub = this.route
-      .queryParams
-      .subscribe((params) => {
-        if (params) {
-          if (params.programType || params.department) {
-            this.initParams();
-            this.params = params;
-            const searchParams = this.getQueryParams();
-            if (params.resetFilter && params.resetFilter === 'true') {
-                this.dailyVisitsPatientList = [];
-            } else {
-               this.getDailyVisits(searchParams);
-            }
-          } else {
+    const routeSub = this.route.queryParams.subscribe((params) => {
+      if (params) {
+        if (params.programType || params.department) {
+          this.initParams();
+          this.params = params;
+          const searchParams = this.getQueryParams();
+          if (params.resetFilter && params.resetFilter === 'true') {
             this.dailyVisitsPatientList = [];
+          } else {
+            this.getDailyVisits(searchParams);
           }
-
+        } else {
+          this.dailyVisitsPatientList = [];
         }
-      });
+      }
+    });
     this.subs.push(routeSub);
   }
 
   public loadMoreVisits() {
-
     this.loadingDailyVisits = true;
     this.clinicDashboardCacheService.setIsLoading(this.loadingDailyVisits);
     const params = this.getQueryParams();
     this.getDailyVisits(params);
-
   }
 
   public getQueryParams() {
@@ -125,7 +121,7 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
       department = this.params.department;
     }
     if (this.params.programType && this.params.programType.length > 0) {
-        programType = this.params.programType;
+      programType = this.params.programType;
     }
     if (this.params.visitType && this.params.visitType.length > 0) {
       visitType = this.params.visitType;
@@ -134,11 +130,9 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
       encounterType = this.params.encounterType;
     }
     if (this.params.startDate) {
-
       this.selectedDate = this.params.startDate;
-
     } else {
-       this.selectedDate = Moment().format('YYYY-MM-DD');
+      this.selectedDate = Moment().format('YYYY-MM-DD');
     }
     return {
       startDate: this.selectedDate,
@@ -150,7 +144,6 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
       encounterType: encounterType,
       limit: 1000
     };
-
   }
 
   private initParams() {
@@ -166,8 +159,7 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
     this.setBusy();
     this.loadingDailyVisits = true;
     this.clinicDashboardCacheService.setIsLoading(this.loadingDailyVisits);
-    const result = this.dailyScheduleResource.
-      getDailyVisits(params);
+    const result = this.dailyScheduleResource.getDailyVisits(params);
 
     if (result === null) {
       throw new Error('Null daily appointments observable');
@@ -183,13 +175,16 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
           }
           this.setFree();
           this.loadingDailyVisits = false;
-          this.clinicDashboardCacheService.setIsLoading(this.loadingDailyVisits);
-        }
-        ,
+          this.clinicDashboardCacheService.setIsLoading(
+            this.loadingDailyVisits
+          );
+        },
         (error) => {
           this.setFree();
           this.loadingDailyVisits = false;
-          this.clinicDashboardCacheService.setIsLoading(this.loadingDailyVisits);
+          this.clinicDashboardCacheService.setIsLoading(
+            this.loadingDailyVisits
+          );
           this.dataLoaded = true;
           this.errors.push({
             id: 'Daily Visits',
@@ -201,20 +196,15 @@ export class DailyScheduleVisitsComponent implements OnInit, OnDestroy {
   }
 
   private setBusy() {
-
     this.busyIndicator = {
       busy: true,
       message: 'Please wait...Loading'
     };
-
   }
   private setFree() {
-
     this.busyIndicator = {
       busy: false,
       message: ''
     };
-
   }
-
 }
--- src/app/clinic-schedule-lib/monthly-schedule/monthly-schedule.component.spec.ts
+++ src/app/clinic-schedule-lib/monthly-schedule/monthly-schedule.component.spec.ts
@@ -1,14 +1,27 @@
-
 /* tslint:disable:no-unused-variable */
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { TestBed, async, fakeAsync, ComponentFixture, tick, flush } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  TestBed,
+  async,
+  fakeAsync,
+  ComponentFixture,
+  tick,
+  flush
+} from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 import { Router, ActivatedRoute } from '@angular/router';
 import {
-  MatProgressSpinnerModule, MatProgressBarModule, MatTabsModule,
-  MatSlideToggleModule, MatDatepickerModule, MatNativeDateModule
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatTabsModule,
+  MatSlideToggleModule,
+  MatDatepickerModule,
+  MatNativeDateModule
 } from '@angular/material';
 
 import { Observable, of, throwError } from 'rxjs';
@@ -18,29 +31,22 @@ import { MonthlyScheduleResourceService } from '../../etl-api/monthly-scheduled-
 import { CalendarModule } from 'angular-calendar';
 import { NgxMyDatePickerModule } from 'ngx-mydatepicker';
 import { DateTimePickerModule } from 'ngx-openmrs-formentry/';
-import { ClinicDashboardCacheService
-} from '../../clinic-dashboard/services/clinic-dashboard-cache.service';
+import { ClinicDashboardCacheService } from '../../clinic-dashboard/services/clinic-dashboard-cache.service';
 import { AppSettingsService } from '../../app-settings/app-settings.service';
 import { LocalStorageService } from '../../utils/local-storage.service';
 import { DataCacheService } from '../../shared/services/data-cache.service';
 import { CacheModule } from 'ionic-cache/dist/cache.module';
 import { AppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytics.service';
 import { FakeAppFeatureAnalytics } from '../../shared/app-analytics/app-feature-analytcis.mock';
-import {
-  ProgramVisitEncounterSearchComponent
-} from './../../program-visit-encounter-search/program-visit-encounter-search.component';
+import { ProgramVisitEncounterSearchComponent } from './../../program-visit-encounter-search/program-visit-encounter-search.component';
 import { PatientProgramResourceService } from './../../etl-api/patient-program-resource.service';
-import { AngularMultiSelectModule
-} from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
-import { DepartmentProgramsConfigService
-} from './../../etl-api/department-programs-config.service';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
+import { DepartmentProgramsConfigService } from './../../etl-api/department-programs-config.service';
 import { SelectDepartmentService } from './../../shared/services/select-department.service';
 class DataStub {
-
   public getMonthlySchedule(payload): Observable<any> {
     return Observable.of({ status: 'okay' });
   }
-
 }
 class ClinicDashboardCacheServiceStub {
   public getCurrentClinic() {
@@ -48,174 +54,174 @@ class ClinicDashboardCacheServiceStub {
   }
 }
 const results = {
-  'results': [
+  results: [
     {
-      'date': '2017-02-01',
-      'count': {
-        'attended': 84,
-        'scheduled': 102,
-        'not_attended': 37,
-        'has_not_returned': 23
+      date: '2017-02-01',
+      count: {
+        attended: 84,
+        scheduled: 102,
+        not_attended: 37,
+        has_not_returned: 23
       }
     },
     {
-      'date': '2017-02-02',
-      'count': {
-        'attended': 93,
-        'scheduled': 119,
-        'not_attended': 33,
-        'has_not_returned': 32
+      date: '2017-02-02',
+      count: {
+        attended: 93,
+        scheduled: 119,
+        not_attended: 33,
+        has_not_returned: 32
       }
     },
     {
-      'date': '2017-02-03',
-      'count': {
-        'attended': 30,
-        'scheduled': 27,
-        'not_attended': 10,
-        'has_not_returned': 11
+      date: '2017-02-03',
+      count: {
+        attended: 30,
+        scheduled: 27,
+        not_attended: 10,
+        has_not_returned: 11
       }
     },
     {
-      'date': '2017-02-06',
-      'count': {
-        'attended': 61,
-        'scheduled': 74,
-        'not_attended': 22,
-        'has_not_returned': 24
+      date: '2017-02-06',
+      count: {
+        attended: 61,
+        scheduled: 74,
+        not_attended: 22,
+        has_not_returned: 24
       }
     },
     {
-      'date': '2017-02-07',
-      'count': {
-        'attended': 72,
-        'scheduled': 89,
-        'not_attended': 19,
-        'has_not_returned': 29
+      date: '2017-02-07',
+      count: {
+        attended: 72,
+        scheduled: 89,
+        not_attended: 19,
+        has_not_returned: 29
       }
     },
     {
-      'date': '2017-02-08',
-      'count': {
-        'attended': 96,
-        'scheduled': 98,
-        'not_attended': 19,
-        'has_not_returned': 23
+      date: '2017-02-08',
+      count: {
+        attended: 96,
+        scheduled: 98,
+        not_attended: 19,
+        has_not_returned: 23
       }
     },
     {
-      'date': '2017-02-09',
-      'count': {
-        'attended': 82,
-        'scheduled': 86,
-        'not_attended': 11,
-        'has_not_returned': 30
+      date: '2017-02-09',
+      count: {
+        attended: 82,
+        scheduled: 86,
+        not_attended: 11,
+        has_not_returned: 30
       }
     },
     {
-      'date': '2017-02-10',
-      'count': {
-        'attended': 29,
-        'scheduled': 13,
-        'not_attended': 3,
-        'has_not_returned': 4
+      date: '2017-02-10',
+      count: {
+        attended: 29,
+        scheduled: 13,
+        not_attended: 3,
+        has_not_returned: 4
       }
     },
     {
-      'date': '2017-02-13',
-      'count': {
-        'attended': 35,
-        'scheduled': 81,
-        'not_attended': 12,
-        'has_not_returned': 0
+      date: '2017-02-13',
+      count: {
+        attended: 35,
+        scheduled: 81,
+        not_attended: 12,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-14',
-      'count': {
-        'scheduled': 117,
-        'not_attended': 12,
-        'has_not_returned': 0
+      date: '2017-02-14',
+      count: {
+        scheduled: 117,
+        not_attended: 12,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-15',
-      'count': {
-        'scheduled': 109,
-        'not_attended': 16,
-        'has_not_returned': 0
+      date: '2017-02-15',
+      count: {
+        scheduled: 109,
+        not_attended: 16,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-16',
-      'count': {
-        'scheduled': 126,
-        'not_attended': 18,
-        'has_not_returned': 0
+      date: '2017-02-16',
+      count: {
+        scheduled: 126,
+        not_attended: 18,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-17',
-      'count': {
-        'scheduled': 24,
-        'not_attended': 5,
-        'has_not_returned': 0
+      date: '2017-02-17',
+      count: {
+        scheduled: 24,
+        not_attended: 5,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-20',
-      'count': {
-        'scheduled': 65,
-        'not_attended': 5,
-        'has_not_returned': 0
+      date: '2017-02-20',
+      count: {
+        scheduled: 65,
+        not_attended: 5,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-21',
-      'count': {
-        'scheduled': 110,
-        'not_attended': 12,
-        'has_not_returned': 0
+      date: '2017-02-21',
+      count: {
+        scheduled: 110,
+        not_attended: 12,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-22',
-      'count': {
-        'scheduled': 102,
-        'not_attended': 4,
-        'has_not_returned': 0
+      date: '2017-02-22',
+      count: {
+        scheduled: 102,
+        not_attended: 4,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-23',
-      'count': {
-        'scheduled': 107,
-        'not_attended': 4,
-        'has_not_returned': 0
+      date: '2017-02-23',
+      count: {
+        scheduled: 107,
+        not_attended: 4,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-24',
-      'count': {
-        'scheduled': 17,
-        'not_attended': 1,
-        'has_not_returned': 0
+      date: '2017-02-24',
+      count: {
+        scheduled: 17,
+        not_attended: 1,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-27',
-      'count': {
-        'scheduled': 73,
-        'not_attended': 6,
-        'has_not_returned': 0
+      date: '2017-02-27',
+      count: {
+        scheduled: 73,
+        not_attended: 6,
+        has_not_returned: 0
       }
     },
     {
-      'date': '2017-02-28',
-      'count': {
-        'scheduled': 111,
-        'not_attended': 9,
-        'has_not_returned': 0
+      date: '2017-02-28',
+      count: {
+        scheduled: 111,
+        not_attended: 9,
+        has_not_returned: 0
       }
     }
   ]
@@ -224,7 +230,7 @@ class MockRouter {
   public navigate = jasmine.createSpy('navigate');
 }
 class MockActivatedRoute {
-  public params = Observable.of([{ 'id': 1 }]);
+  public params = Observable.of([{ id: 1 }]);
   public snapshot = {
     queryParams: { date: '' }
   };
@@ -238,9 +244,13 @@ describe('MonthlyScheduleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [CalendarModule.forRoot(),
-        BrowserAnimationsModule, CacheModule, AngularMultiSelectModule,
-        FormsModule, MatTabsModule,
+      imports: [
+        CalendarModule.forRoot(),
+        BrowserAnimationsModule,
+        CacheModule,
+        AngularMultiSelectModule,
+        FormsModule,
+        MatTabsModule,
         MatProgressSpinnerModule,
         MatProgressBarModule,
         MatDatepickerModule,
@@ -251,7 +261,10 @@ describe('MonthlyScheduleComponent', () => {
         NgxMyDatePickerModule.forRoot(),
         DateTimePickerModule
       ],
-      declarations: [MonthlyScheduleBaseComponent, ProgramVisitEncounterSearchComponent],
+      declarations: [
+        MonthlyScheduleBaseComponent,
+        ProgramVisitEncounterSearchComponent
+      ],
       providers: [
         MonthlyScheduleResourceService,
         ClinicDashboardCacheService,
@@ -271,13 +284,17 @@ describe('MonthlyScheduleComponent', () => {
           useClass: FakeAppFeatureAnalytics
         }
       ]
-    }).compileComponents()
+    })
+      .compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(MonthlyScheduleBaseComponent);
         comp = fixture.componentInstance;
-        dataStub = fixture.debugElement.injector.get(MonthlyScheduleResourceService);
-        departmentProgConfigService = fixture.debugElement.injector.
-          get(DepartmentProgramsConfigService);
+        dataStub = fixture.debugElement.injector.get(
+          MonthlyScheduleResourceService
+        );
+        departmentProgConfigService = fixture.debugElement.injector.get(
+          DepartmentProgramsConfigService
+        );
       });
   }));
   afterEach(() => {
@@ -320,5 +337,4 @@ describe('MonthlyScheduleComponent', () => {
     fixture.detectChanges();
     expect(events[0].title).toEqual(84);
   });
-
 });
