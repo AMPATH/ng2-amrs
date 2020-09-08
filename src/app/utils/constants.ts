d97b570ea3d3a7655618f74d7264e3858818a5cc
Dennis Kigen <kigen.work@gmail.com>
2020-09-09 00:43:16 +0300

    Format src/app/utils

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,6 +1,4 @@
-
 export class Constants {
-
   public static readonly CREDENTIALS_KEY = 'auth.credentials';
 
   public static readonly USER_KEY = 'user';
--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,41 +1,31 @@
 import * as _ from 'lodash';
 
 export class Helpers {
-
   public static isNullOrUndefined(subject) {
-
-    return _.isUndefined(subject)
-      || _.isNull(subject)
-      || (typeof subject === 'string'
-        && (subject.length === 0
-          || !subject.trim()));
-
+    return (
+      _.isUndefined(subject) ||
+      _.isNull(subject) ||
+      (typeof subject === 'string' && (subject.length === 0 || !subject.trim()))
+    );
   }
 
   public static formatBlankOrNull(obj, text) {
-
     _.each(Object.keys(obj), (key) => {
-
       if (obj[key] === '' || obj[key] === null) {
         obj[key] = text;
       }
-
     });
-
   }
 
   public static isNullOrEmpty(str) {
-    return (!str || 0 === str.length);
+    return !str || 0 === str.length;
   }
 
   public static hasAllMembersUndefinedOrNull(obj, members) {
-
     let hasANonNullMember = false;
 
     for (const member of members) {
-
       if (!this.isNullOrUndefined(obj[member])) {
-
         hasANonNullMember = true;
         break;
       }
@@ -44,6 +34,5 @@ export class Helpers {
     return !hasANonNullMember;
   }
 
-  constructor() { }
-
+  constructor() {}
 }
