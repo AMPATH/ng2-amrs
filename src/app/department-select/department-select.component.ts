a389aa30e70e8fb3ae838631a830571f9c733417
Dennis Kigen <kigen.work@gmail.com>
2020-09-08 23:49:31 +0300

    Format src/app/department-select

--- src/app/department-select/department-select.component.ts
+++ src/app/department-select/department-select.component.ts
@@ -4,57 +4,59 @@ import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 
 @Component({
-    selector: 'department-select',
-    templateUrl: 'department-select.component.html',
-    styleUrls: ['./department-select.component.css']
-  })
+  selector: 'department-select',
+  templateUrl: 'department-select.component.html',
+  styleUrls: ['./department-select.component.css']
+})
 export class DepartmentSelectComponent implements OnInit {
+  public departments: any;
+  public currentDepartment = '';
+  public dashRoute: string;
 
-public departments: any;
-public currentDepartment = '';
-public dashRoute: string;
-
-constructor(
+  constructor(
     public locaStorageService: LocalStorageService,
-    public router: Router) {}
+    public router: Router
+  ) {}
 
-public ngOnInit() {
+  public ngOnInit() {
     this.getCurrentDepartment();
-}
-public clinicDashboardDepts(departments, route) {
+  }
+  public clinicDashboardDepts(departments, route) {
     this.dashRoute = route;
     this.departments = [];
     _.each(departments, (department: any) => {
-        if (department.baseRoute !== 'general') {
-
-            const specDept = {
-                'name': department.departmentName,
-                'baseRoute':  department.baseRoute
-            };
-            this.departments.push(specDept);
-
-        }
+      if (department.baseRoute !== 'general') {
+        const specDept = {
+          name: department.departmentName,
+          baseRoute: department.baseRoute
+        };
+        this.departments.push(specDept);
+      }
     });
-}
+  }
 
-public setDefaultDepartment(department) {
-    const departmentObj = [{
-       'itemName': department.name,
-       'id': ''
-    }];
-    this.locaStorageService.setItem('userDefaultDepartment', JSON.stringify(departmentObj) );
+  public setDefaultDepartment(department) {
+    const departmentObj = [
+      {
+        itemName: department.name,
+        id: ''
+      }
+    ];
+    this.locaStorageService.setItem(
+      'userDefaultDepartment',
+      JSON.stringify(departmentObj)
+    );
     this.router.navigate([this.dashRoute]);
+  }
 
-}
-
-public getCurrentDepartment() {
-    const currentDepartmentObj: any = JSON.parse(this.locaStorageService.getItem('userDefaultDepartment'));
+  public getCurrentDepartment() {
+    const currentDepartmentObj: any = JSON.parse(
+      this.locaStorageService.getItem('userDefaultDepartment')
+    );
     if (typeof currentDepartmentObj !== 'undefined') {
-        this.currentDepartment = currentDepartmentObj[0].itemName;
+      this.currentDepartment = currentDepartmentObj[0].itemName;
     } else {
-        this.currentDepartment = 'HIV';
+      this.currentDepartment = 'HIV';
     }
-
-}
-
+  }
 }
