a3570b84c399b83b8951e42feaa5d4f2c17882d8
Dennis Kigen <kigen.work@gmail.com>
2020-09-09 00:40:30 +0300

    Format src/app/user-default-properties

--- src/app/user-default-properties/user-default-properties.component.ts
+++ src/app/user-default-properties/user-default-properties.component.ts
@@ -7,9 +7,7 @@ import { User } from '../models/user.model';
 import { UserDefaultPropertiesService } from './user-default-properties.service';
 import { LocalStorageService } from '../utils/local-storage.service';
 import { DepartmentProgramsConfigService } from './../etl-api/department-programs-config.service';
-import {
-  RetrospectiveDataEntryService
-} from '../retrospective-data-entry/services/retrospective-data-entry.service';
+import { RetrospectiveDataEntryService } from '../retrospective-data-entry/services/retrospective-data-entry.service';
 import * as _ from 'lodash';
 
 @Component({
@@ -18,7 +16,6 @@ import * as _ from 'lodash';
   styleUrls: ['./user-default-properties.component.css']
 })
 export class UserDefaultPropertiesComponent implements OnInit {
-
   public isBusy: boolean = false;
   public query: string = '';
   public user: User;
@@ -34,37 +31,36 @@ export class UserDefaultPropertiesComponent implements OnInit {
   public disable = false;
   private retroSettings: any;
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private userService: UserService,
-              private localStorageService: LocalStorageService,
-              private departmentProgramService: DepartmentProgramsConfigService,
-              private retrospectiveDataEntryService: RetrospectiveDataEntryService,
-              private propertyLocationService: UserDefaultPropertiesService
-  ) {
-
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private localStorageService: LocalStorageService,
+    private departmentProgramService: DepartmentProgramsConfigService,
+    private retrospectiveDataEntryService: RetrospectiveDataEntryService,
+    private propertyLocationService: UserDefaultPropertiesService
+  ) {}
 
   public ngOnInit() {
-
     this.isBusy = true;
     // if the user is confirming, prefill the current location
     this.route.params.subscribe((params: Params) => {
       if (params['confirm'] !== undefined) {
-        this.location = this.retrospectiveDataEntryService.mappedLocation(this.currentLocation);
-        this.propertyLocationService.setUserProperty('retroLocation',
-          JSON.stringify(this.location));
+        this.location = this.retrospectiveDataEntryService.mappedLocation(
+          this.currentLocation
+        );
+        this.propertyLocationService.setUserProperty(
+          'retroLocation',
+          JSON.stringify(this.location)
+        );
       }
       this.getDepartmentsLocations();
     });
-
   }
-
 
   public goToPatientSearch() {
     this.isLoading = true;
     this.router.navigate(['patient-dashboard/patient-search']);
-
   }
 
   public selectDepartment(event: any) {
@@ -74,59 +70,77 @@ export class UserDefaultPropertiesComponent implements OnInit {
 
     const department = [deptObject];
     this.selectedDepartment = event;
-    this.localStorageService.setItem('userDefaultDepartment', JSON.stringify(department));
+    this.localStorageService.setItem(
+      'userDefaultDepartment',
+      JSON.stringify(department)
+    );
   }
 
   public select(item: any) {
     this.disable = false;
-    const location = JSON.stringify({uuid: item.value, display: item.label});
+    const location = JSON.stringify({ uuid: item.value, display: item.label });
     this.currentLocation = location;
-    this.propertyLocationService.setUserProperty('userDefaultLocation', location);
-    this.propertyLocationService.setUserProperty('retroLocation', JSON.stringify(item));
+    this.propertyLocationService.setUserProperty(
+      'userDefaultLocation',
+      location
+    );
+    this.propertyLocationService.setUserProperty(
+      'retroLocation',
+      JSON.stringify(item)
+    );
   }
 
   private getDepartmentsLocations() {
-    this.departmentProgramService.getDartmentProgramsConfig().pipe(take(1)).subscribe((results) => {
-      if (results) {
-        this.mapDepartments(results);
-        this.setDepartmentLocation();
-      }
-      this.currentLocation = this.propertyLocationService.getCurrentUserDefaultLocationObject();
-      if (!this.currentLocation) {
-        this.disable = true;
-      }
-    });
+    this.departmentProgramService
+      .getDartmentProgramsConfig()
+      .pipe(take(1))
+      .subscribe((results) => {
+        if (results) {
+          this.mapDepartments(results);
+          this.setDepartmentLocation();
+        }
+        this.currentLocation = this.propertyLocationService.getCurrentUserDefaultLocationObject();
+        if (!this.currentLocation) {
+          this.disable = true;
+        }
+      });
   }
 
   private setDepartmentLocation() {
-    this.propertyLocationService.getLocations().pipe(take(1)).subscribe((response) => {
-      this.locations = response.results.map((location: any) => {
-        if (location && !_.isNil(location.display)) {
-          return this.retrospectiveDataEntryService.mappedLocation(location);
+    this.propertyLocationService
+      .getLocations()
+      .pipe(take(1))
+      .subscribe((response) => {
+        this.locations = response.results.map((location: any) => {
+          if (location && !_.isNil(location.display)) {
+            return this.retrospectiveDataEntryService.mappedLocation(location);
+          }
+        });
+        this.isBusy = false;
+        const department = JSON.parse(
+          this.localStorageService.getItem('userDefaultDepartment')
+        );
+        if (department !== null) {
+          this.selectedDepartment = department[0].itemName;
+        } else {
+          this.selectedDepartment = 'HIV';
+          setTimeout(() => {
+            this.selectDepartment(this.selectedDepartment);
+          }, 1000);
         }
+        this.retrospectiveDataEntryService.retroSettings.subscribe(
+          (retroSettings) => {
+            this.retroSettings = retroSettings;
+          }
+        );
       });
-      this.isBusy = false;
-      const department = JSON.parse(this.localStorageService.getItem('userDefaultDepartment'));
-      if (department !== null) {
-        this.selectedDepartment = department[0].itemName;
-      } else {
-        this.selectedDepartment = 'HIV';
-        setTimeout(() => {
-          this.selectDepartment(this.selectedDepartment);
-        }, 1000);
-
-      }
-      this.retrospectiveDataEntryService.retroSettings.subscribe((retroSettings) => {
-        this.retroSettings = retroSettings;
-      });
-    });
   }
 
   private mapDepartments(results: any[]) {
     _.each(results, (department, key: string) => {
       const dept = {
-        'itemName': department.name,
-        'id': key
+        itemName: department.name,
+        id: key
       };
       this.departments.push(dept);
       this.departments = _.filter(this.departments, (dep) => {
@@ -134,5 +148,4 @@ export class UserDefaultPropertiesComponent implements OnInit {
       });
     });
   }
-
 }
