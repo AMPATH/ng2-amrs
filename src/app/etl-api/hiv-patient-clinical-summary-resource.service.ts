6f59ea8b2887067c897cfd3a0442a80873dd3079
Felix Maiko <maikofelix89@gmail.com>
2020-11-11 12:51:25 +0300

    HIV-704: Fix clinical hiv summary not showing in some patients (#1341)

--- src/app/etl-api/hiv-patient-clinical-summary-resource.service.ts
+++ src/app/etl-api/hiv-patient-clinical-summary-resource.service.ts
@@ -14,8 +14,8 @@ export class HivPatientClinicalSummaryResourceService {
       '/patient/' + patientUuid + '/hiv-patient-clinical-summary';
 
     const params: HttpParams = new HttpParams()
-    .set('startIndex', 0 as any as string)
-    .set('limit', 10 as any as string);
+      .set('startIndex', (0 as any) as string)
+      .set('limit', (20 as any) as string);
 
     return this.http.get(api, {params: params});
   }
