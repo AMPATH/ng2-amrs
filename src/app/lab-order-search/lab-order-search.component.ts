1fdfec01884977876739b5b59d8695e035ffe85a
jecihjoy <jecihjoy@gmail.com>
2021-02-09 15:24:16 +0300

    Add informative error message when the order number is invalid (#1357)

--- src/app/lab-order-search/lab-order-search.component.ts
+++ src/app/lab-order-search/lab-order-search.component.ts
@@ -1,5 +1,4 @@
-
-import {take} from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import { Component, Output, OnInit, EventEmitter, Input } from '@angular/core';
 import { OrderResourceService } from '../openmrs-api/order-resource.service';
 
@@ -36,19 +35,19 @@ export class LabOrderSearchComponent implements OnInit {
 
   private hasBeenSearched = false;
 
-  private customOrderObjectDefinition: string = 'custom:(display,uuid,orderNumber,voided,' +
+  private customOrderObjectDefinition: string =
+    'custom:(display,uuid,orderNumber,voided,' +
     'accessionNumber,orderReason,orderReasonNonCoded,urgency,action,commentToFulfiller,' +
     'dateActivated,instructions,orderer:default,encounter:full,patient:(uuid,display,' +
     'identifiers:(identifier,uuid,preferred,' +
     'identifierType:(uuid,name,format,formatDescription,validator)),' +
     'person:(uuid,display,gender,birthdate,dead,age,deathDate,' +
-    'causeOfDeath,preferredName:(uuid,preferred,givenName,middleName,familyName),'
-    + 'attributes,preferredAddress:(uuid,preferred,address1,address2,cityVillage,' +
+    'causeOfDeath,preferredName:(uuid,preferred,givenName,middleName,familyName),' +
+    'attributes,preferredAddress:(uuid,preferred,address1,address2,cityVillage,' +
     'stateProvince,country,postalCode,countyDistrict,address3,address4,address5,' +
     'address6))),concept:ref)';
 
-  constructor(private orderResourceService: OrderResourceService) {
-  }
+  constructor(private orderResourceService: OrderResourceService) {}
 
   public ngOnInit() {
     if (window.innerWidth <= 768) {
@@ -91,28 +90,33 @@ export class LabOrderSearchComponent implements OnInit {
     this.isResetButton = false;
     this.hasBeenSearched = true;
     this.isLoading = true;
-    this.orderResourceService.searchOrdersById(this.orderId, false,
-      this.customOrderObjectDefinition).pipe(
-      take(1)).subscribe((resp) => {
-        if (resp && resp.orderVoided) {
-          this.orderDeleted = true;
-        } else {
+    this.orderResourceService
+      .searchOrdersById(this.orderId, false, this.customOrderObjectDefinition)
+      .pipe(take(1))
+      .subscribe(
+        (resp) => {
+          if (resp && resp.orderVoided) {
+            this.orderDeleted = true;
+          } else {
+            this.orderDeleted = false;
+            this.onOrderRecieved.emit(resp);
+          }
+          this.hasBeenSearched = false;
+          this.isLoading = false;
+          this.hasError = false;
+          this.isResetButton = true;
+          // this.resetSearch();
+        },
+        (err) => {
+          this.onOrderRecieved.emit(null);
+          this.hasError = true;
+          this.isLoading = false;
           this.orderDeleted = false;
-          this.onOrderRecieved.emit(resp);
+          this.hasBeenSearched = false;
+          this.isResetButton = true;
+          this.resetInputMargin();
         }
-        this.hasBeenSearched = false;
-        this.isLoading = false;
-        this.hasError = false;
-        this.isResetButton = true;
-        // this.resetSearch();
-      }, (err) => {
-        this.hasError = true;
-        this.isLoading = false;
-        this.orderDeleted = false;
-        this.hasBeenSearched = false;
-        this.isResetButton = true;
-        this.resetInputMargin();
-      });
+      );
   }
 
   public resetInputMargin() {
@@ -135,5 +139,4 @@ export class LabOrderSearchComponent implements OnInit {
     this.resetInputMargin();
     this.onReset.emit();
   }
-
 }
