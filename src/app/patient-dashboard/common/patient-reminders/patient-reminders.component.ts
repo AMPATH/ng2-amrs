5525d68f23404c22dd633895686518ebe3e3119d
jecihjoy <jecihjoy@gmail.com>
2021-03-19 15:24:34 +0300

    HIV-754: Fix reminder action bug issue in information only reminders (#1366)

--- src/app/patient-dashboard/common/patient-reminders/patient-reminders.component.ts
+++ src/app/patient-dashboard/common/patient-reminders/patient-reminders.component.ts
@@ -96,6 +96,8 @@ export class PatientRemindersComponent implements OnInit, OnDestroy {
     _.each(reminders, (reminder: any) => {
       if (reminder.action) {
         this.toastrConfig.reminder = reminder;
+      } else {
+        this.toastrConfig.reminder = null;
       }
       if (reminder.type === 'success') {
         toast = this.toastrService.success(reminder.message, reminder.title, this.toastrConfig);
