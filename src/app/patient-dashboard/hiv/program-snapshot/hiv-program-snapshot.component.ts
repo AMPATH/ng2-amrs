20f35386b3a5081387cc45398ae3010b92a7929a
Felix Maiko <maikofelix89@gmail.com>
2021-06-29 14:10:44 +0300

    Fix for Cervical cancer summary on empty result (#1396)

--- src/app/patient-dashboard/hiv/program-snapshot/hiv-program-snapshot.component.ts
+++ src/app/patient-dashboard/hiv/program-snapshot/hiv-program-snapshot.component.ts
@@ -64,7 +64,7 @@ export class HivProgramSnapshotComponent implements OnInit {
   public isMoriskyScorePoorOrInadequate = false;
   public hivDisclosureStatus: any;
   public latestCervicalScreeningSummary = [];
-  public cervicalScreeningSummary: any;
+  public cervicalScreeningSummary = [];
   private obs: any[] = [];
 
   constructor(
