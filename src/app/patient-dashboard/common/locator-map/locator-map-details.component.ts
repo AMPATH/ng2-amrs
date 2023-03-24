6a18997a6c56bbfbdb45b2249363d57801283028
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2023-03-24 16:45:01 +0300

    Added locator map details component

---
+++ src/app/patient-dashboard/common/locator-map/locator-map-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'locator-map-details',
+  templateUrl: './locator-map-details.component.html',
+  styleUrls: ['./locator-map-details.component.css']
+})
+export class LocatorMapDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
