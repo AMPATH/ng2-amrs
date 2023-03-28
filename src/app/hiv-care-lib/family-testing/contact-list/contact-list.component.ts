6f895e60b4af2f271a60febaca2c0c077430340f
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2023-03-28 11:22:49 +0300

    Revert "added reported test date and result to family history"
    This reverts commit 3f051856ec1e5e298a0959d728e7e7ce2c144f69.

--- src/app/hiv-care-lib/family-testing/contact-list/contact-list.component.ts
+++ src/app/hiv-care-lib/family-testing/contact-list/contact-list.component.ts
@@ -52,7 +52,7 @@ export class ContactListComponent implements OnInit {
     },
 
     {
-      field: 'modified_fm_status',
+      field: 'modified_test_result',
       headerName: 'Current test results',
       width: 150
     },
