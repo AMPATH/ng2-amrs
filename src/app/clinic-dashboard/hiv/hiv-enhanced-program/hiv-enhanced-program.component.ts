773ef4492ea6857c3d7dfae06f54c901b0cc9829
Kipchumba Bett <kbett68@gmail.com>
2023-05-16 11:47:56 +0300

    POC-285: Follow-up > only patient with VL > 200 eligible for viremia (#1608)

--- src/app/clinic-dashboard/hiv/hiv-enhanced-program/hiv-enhanced-program.component.ts
+++ src/app/clinic-dashboard/hiv/hiv-enhanced-program/hiv-enhanced-program.component.ts
@@ -28,12 +28,8 @@ export class HivEnhancedComponent implements OnInit {
   };
   public viremiaStatus = {
     all: {
-      lowerVl: '50',
+      lowerVl: '200',
       upperVl: ''
-    },
-    lowRiskLowLevelViremia: {
-      lowerVl: '50',
-      upperVl: '199'
     },
     highRiskLowLevelViremia: {
       lowerVl: '200',
@@ -136,9 +132,9 @@ export class HivEnhancedComponent implements OnInit {
     ];
   }
 
-  public generateReport(indicator, lowerVl = '50', upperVl = '') {
+  public generateReport(indicator, lowerVl = '200', upperVl = '') {
     if (this.lowerVl == null) {
-      lowerVl = '50';
+      lowerVl = '200';
       upperVl = '';
     } else {
       lowerVl = this.lowerVl;
