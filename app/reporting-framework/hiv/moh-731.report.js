f22b15432e85bfd29fb86f17b46f7386bc76d0d9
Drizzentic <derrickrono@gmail.com>
2023-07-04 15:12:44 +0300

    Fix issue with aggregated in Reports (#1290)

--- app/reporting-framework/hiv/moh-731.report.js
+++ app/reporting-framework/hiv/moh-731.report.js
@@ -15,7 +15,7 @@ export class Moh731Report extends MultiDatasetPatientlistReport {
     }
     params.hivMonthlyDatasetSource = 'etl.hiv_monthly_report_dataset_frozen'; // defaults to frozen
     params.hivVlDataSource =
-      '(SELECT fli.person_id, fli.hiv_viral_load as vl_1, fli.test_datetime as vl_1_date FROM etl.flat_labs_and_imaging fli INNER JOIN (SELECT person_id, MAX(test_datetime) AS max_vl_1_date FROM etl.flat_labs_and_imaging fli where fli.hiv_viral_load is not null GROUP BY person_id) max_dates ON fli.person_id = max_dates.person_id AND fli.test_datetime = max_dates.max_vl_1_date and fli.hiv_viral_load is not null)';
+      '(SELECT fli.person_id, fli.hiv_viral_load as vl_1, fli.test_datetime as vl_1_date FROM etl.flat_labs_and_imaging fli INNER JOIN (SELECT person_id, MAX(test_datetime) AS max_vl_1_date,max(encounter_id) as encounter_id FROM etl.flat_labs_and_imaging fli where fli.hiv_viral_load is not null GROUP BY person_id) max_dates ON fli.person_id = max_dates.person_id AND fli.encounter_id = max_dates.encounter_id)';
     console.log('creating new moh 731 report service');
     super(reportName, params);
   }
