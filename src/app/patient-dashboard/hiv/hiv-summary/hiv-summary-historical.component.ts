d52a4693d4f44c570fc0ac48b7e4fb38a37ef2b9
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2023-08-15 17:13:45 +0300

    added patient tracker

--- src/app/patient-dashboard/hiv/hiv-summary/hiv-summary-historical.component.ts
+++ src/app/patient-dashboard/hiv/hiv-summary/hiv-summary-historical.component.ts
@@ -6,6 +6,7 @@ import { PatientService } from '../../services/patient.service';
 import { HivSummaryService } from './hiv-summary.service';
 import { Patient } from '../../../models/patient.model';
 import { Subscription } from 'rxjs';
+import { result } from 'lodash';
 
 @Component({
   selector: 'hiv-summary-historical',
@@ -26,6 +27,8 @@ export class HivSummaryHistoricalComponent implements OnInit, OnDestroy {
   public hasMedicationRtc = false;
   public hasMdtSessionNo = false;
   isHEIActive: boolean;
+  public showMissedDays: boolean;
+  public patientStatus: any;
   public daysMissed = 0;
 
   constructor(
@@ -104,10 +107,19 @@ export class HivSummaryHistoricalComponent implements OnInit, OnDestroy {
                   const encounter_date = new Date(hivsum.encounter_datetime);
                   const startDate = Moment(encounter_date, 'YYYY-MM-DD');
                   const endDate = Moment(prev_rtc, 'YYYY-MM-DD');
+                  const specificDate = Moment('2005-01-01');
+                  console.log('Data', data);
+
+                  if (endDate < specificDate) {
+                    this.showMissedDays = false;
+                  } else {
+                    this.showMissedDays = true;
+                  }
                   const dateDiffInDays = endDate.diff(startDate, 'days');
                   if (dateDiffInDays > 0) {
                     this.daysMissed = dateDiffInDays;
                   }
+
                   if (this.hasMdtSessionNo === false) {
                     this.hasMdtSessionNo = this.hasColumnData(
                       data[r],
@@ -136,6 +148,7 @@ export class HivSummaryHistoricalComponent implements OnInit, OnDestroy {
       );
     this.subscription.push(summarySub);
   }
+
   public loadMoreHivSummary() {
     this.isLoading = true;
     this.loadHivSummary(this.patientUuid, this.nextStartIndex);
