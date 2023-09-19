c9732272a24fb0b1431f1f0f78f686f0512a25e8
Angie-540 <janiceangella21@gmail.com>
2023-09-19 09:15:54 +0300

    interface

---
+++ src/app/patient-dashboard/hiv/hiv-summary/ahd-events-summary/ahd-events-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ahd-events-summary',
+  templateUrl: './ahd-events-summary.component.html',
+  styleUrls: ['./ahd-events-summary.component.css']
+})
+export class AhdEventsSummaryComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
