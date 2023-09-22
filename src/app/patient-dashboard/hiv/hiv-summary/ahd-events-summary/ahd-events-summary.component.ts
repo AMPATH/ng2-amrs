ab20531dd6f2d68e45d083db5d8e4d1c66d36ceb
Angie-540 <janiceangella21@gmail.com>
2023-09-22 09:28:09 +0300

    ahd-events

--- src/app/patient-dashboard/hiv/hiv-summary/ahd-events-summary/ahd-events-summary.component.ts
+++ src/app/patient-dashboard/hiv/hiv-summary/ahd-events-summary/ahd-events-summary.component.ts
@@ -1,4 +1,10 @@
+import { HivSummaryComponent } from './../hiv-summary.component';
 import { Component, OnInit } from '@angular/core';
+import { HivSummaryResourceService } from '../../../etl-api/hiv-summary-resource.service';
+import { EncounterResourceService } from '../../../openmrs-api/encounter-resource.service';
+import { PatientService } from '../../services/patient.service';
+import { Subscription } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-ahd-events-summary',
@@ -6,7 +12,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ahd-events-summary.component.css']
 })
 export class AhdEventsSummaryComponent implements OnInit {
-  constructor() {}
+  public encounters = [];
+  public patient: any;
+  public errors: any;
+  public subscription: Subscription;
+
+  constructor(
+    private patientService: PatientService,
+    private hivSummaryResourceService: HivSummaryResourceService,
+    private encounterResourceService: EncounterResourceService
+  ) {}
 
   ngOnInit() {}
+
+  public getPatientTbStatus(hivSummaryData: any) {
+    const latestStatus = _.orderBy(
+      hivSummaryData,
+      (hivSummary) => {
+        return moment(hivSummary.tb_treatment_start_date);
+      },
+      ['desc']
+    );
+  }
+
+  public getPatient() {
+    const reportName = 'ahd-events-summary';
+    this.subscription = this.patientService.currentlyLoadedPatient.subscribe(
+      (patient) => {
+        if (patient) {
+          this.patient = patient;
+        }
+      },
+      (err) => {
+        this.errors.push({
+          id: 'patient',
+          message: 'error fetching ahd events'
+        });
+      }
+    );
+  }
 }
