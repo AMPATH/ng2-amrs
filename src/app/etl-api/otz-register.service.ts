04c32c9e8cd76f442e024b601e5783565e3c6c7c
Angie-540 <janiceangella21@gmail.com>
2024-03-24 18:55:14 +0300

    POC-698: Added otzRegister service

---
+++ src/app/etl-api/otz-register.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import * as _ from 'lodash';
+
+import { AppSettingsService } from '../app-settings/app-settings.service';
+import { DataCacheService } from '../shared/services/data-cache.service';
+
+@Injectable()
+export class OtzRegisterService {
+  constructor(
+    protected http: HttpClient,
+    protected appSettingsService: AppSettingsService,
+    private cacheService: DataCacheService
+  ) {}
+
+  public getBaseUrl(): string {
+    return this.appSettingsService.getEtlRestbaseurl().trim() + 'otz-register';
+  }
+
+  public getOtzRegister(payload: {
+    endDate: string;
+    locationUuid: string;
+  }): Observable<any> {
+    const url = this.getBaseUrl();
+    let urlParams: HttpParams = new HttpParams().set(
+      'endDate',
+      payload.endDate
+    );
+    // .set('locationUuid', payload.locationUuid);
+    // const request = this.http.get(url);
+
+    return this.http.get(url, { params: urlParams });
+
+    // return this.cacheService.cacheRequest(url, '', request);
+  }
+}
