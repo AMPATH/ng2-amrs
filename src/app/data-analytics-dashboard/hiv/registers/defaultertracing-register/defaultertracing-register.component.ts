a048560fbb728ec5336eb6b279ef602193ea08f3
Alfred-Mutai <mutaialfr@gmail.com>
2024-06-12 11:44:22 +0300

    Updated registers

--- src/app/data-analytics-dashboard/hiv/registers/defaultertracing-register/defaultertracing-register.component.ts
+++ src/app/data-analytics-dashboard/hiv/registers/defaultertracing-register/defaultertracing-register.component.ts
@@ -1,7 +1,20 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  Output,
+  Input,
+  OnDestroy,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as _ from 'lodash';
 import * as Moment from 'moment';
+import { RegistersResourceService } from 'src/app/etl-api/registers-resource.service';
+import * as html2canvas from 'html2canvas';
+import * as jsPDF from 'jspdf';
+import { DataAnalyticsDashboardService } from 'src/app/data-analytics-dashboard/services/data-analytics-dashboard.services';
 @Component({
   selector: 'app-defaultertracing-register',
   templateUrl: './defaultertracing-register.component.html',
@@ -12,7 +25,7 @@ export class DefaultertracingRegisterComponent implements OnInit {
   public params: any;
   public indicators: string;
   public selectedIndicators = [];
-  public txnewReportSummaryData: any = [];
+  public defaulterTracingRegisterData: any = [];
   public columnDefs: any = [];
   public reportName = 'Defaulter Tracing Register';
   public currentView = 'monthly';
@@ -22,6 +35,7 @@ export class DefaultertracingRegisterComponent implements OnInit {
   public quarter: string;
   public eDate: string;
   public sDate: string;
+  public jointLocationUuids: string;
 
   public statusError = false;
   public errorMessage = '';
@@ -32,6 +46,9 @@ export class DefaultertracingRegisterComponent implements OnInit {
   public pinnedBottomRowData: any = [];
   public _month: string;
   public isReleased = true;
+  public generated = false;
+  @ViewChild('defaulterTracingcontentToSnapshot')
+  contentToSnapshot!: ElementRef;
 
   public _locationUuids: any = [];
   public get locationUuids(): Array<string> {
@@ -48,7 +65,13 @@ export class DefaultertracingRegisterComponent implements OnInit {
     this._locationUuids = locationUuids;
   }
 
-  constructor(public router: Router, public route: ActivatedRoute) {
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public register: RegistersResourceService,
+    private datePipe: DatePipe,
+    private dataAnalyticsDashboardService: DataAnalyticsDashboardService
+  ) {
     this.route.queryParams.subscribe((data) => {
       data.month === undefined
         ? (this._month = Moment()
@@ -68,20 +91,27 @@ export class DefaultertracingRegisterComponent implements OnInit {
   }
 
   public generateReport(): any {
+    this.dataAnalyticsDashboardService
+      .getSelectedLocations()
+      .subscribe((data) => {
+        const locationValues = data.locations.map(
+          (location) => `'${location.value}'`
+        );
+        this.jointLocationUuids = locationValues.join(', ');
+      });
+
     this.route.parent.parent.params.subscribe((params: any) => {
-      this.storeParamsInUrl(params.location_uuid);
+      this.storeParamsInUrl();
     });
-    this.txnewReportSummaryData = [];
-    // this.getTxNewReport(this.params);
+    this.defaulterTracingRegisterData = [];
+    this.getPrEPRegisterReport(this.params);
+    this.generated = true;
   }
 
-  public storeParamsInUrl(param) {
+  public storeParamsInUrl() {
     this.params = {
-      locationUuids: param,
-      _month: Moment(this._month).endOf('month').format('YYYY-MM-DD'),
-      month: Moment(this._month).endOf('month').format('YYYY-MM-DD'),
-      reportName: this.reportName,
-      _date: Moment(this._month).format('DD-MM-YYYY')
+      locationUuids: this.jointLocationUuids,
+      month: Moment(this._month).endOf('month').format('YYYY-MM-DD')
     };
     this.router.navigate([], {
       relativeTo: this.route,
@@ -89,54 +119,22 @@ export class DefaultertracingRegisterComponent implements OnInit {
     });
   }
 
-  // public getTxNewReport(params: any) {
-  //   this.isLoading = true;
-  //   this.txnewReport.getTxNewReport(params).subscribe((data) => {
-  //     if (data.error) {
-  //       this.showInfoMessage = true;
-  //       this.errorMessage = `There has been an error while loading the report, please retry again`;
-  //       this.isLoading = false;
-  //     } else {
-  //       this.showInfoMessage = false;
-  //       this.columnDefs = data.sectionDefinitions;
-  //       this.txnewReportSummaryData = data.result;
-  //       this.calculateTotalSummary();
-  //       this.isLoading = false;
-  //       this.showDraftReportAlert(this._month);
-  //     }
-  //   });
-  // }
-
-  public calculateTotalSummary() {
-    const totalsRow = [];
-    if (this.txnewReportSummaryData.length > 0) {
-      const totalObj = {
-        location: 'Totals'
-      };
-      _.each(this.txnewReportSummaryData, (row) => {
-        Object.keys(row).map((key) => {
-          if (Number.isInteger(row[key]) === true) {
-            if (totalObj[key]) {
-              totalObj[key] = row[key] + totalObj[key];
-            } else {
-              totalObj[key] = row[key];
-            }
-          } else {
-            if (Number.isNaN(totalObj[key])) {
-              totalObj[key] = 0;
-            }
-            if (totalObj[key] === null) {
-              totalObj[key] = 0;
-            }
-            totalObj[key] = 0 + totalObj[key];
-          }
-        });
-      });
-      totalObj.location = 'Totals';
-      totalsRow.push(totalObj);
-      this.pinnedBottomRowData = totalsRow;
-    }
+  public getPrEPRegisterReport(params: any) {
+    this.isLoading = true;
+    this.register.getPrEPRegisterReport(params).subscribe((data) => {
+      if (data.error) {
+        this.showInfoMessage = true;
+        this.errorMessage = `There has been an error while loading the report, please retry again`;
+        this.isLoading = false;
+      } else {
+        this.showInfoMessage = false;
+        this.defaulterTracingRegisterData = data;
+        this.isLoading = false;
+        this.showDraftReportAlert(this._month);
+      }
+    });
   }
+
   public onIndicatorSelected(value) {
     this.router.navigate(['patient-list'], {
       relativeTo: this.route,
@@ -157,5 +155,22 @@ export class DefaultertracingRegisterComponent implements OnInit {
     } else {
       this.isReleased = true;
     }
+  }
+
+  transformDate(date: string): string | null {
+    return this.datePipe.transform(date, 'dd/MM/yyyy');
+  }
+
+  public takeSnapshotAndExport() {
+    const elementToSnapshot = this.contentToSnapshot.nativeElement;
+
+    html2canvas(elementToSnapshot).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const imgWidth = 210; // A4 width in mm
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+      pdf.save('Defaulter Tracing Register.pdf');
+    });
   }
 }
