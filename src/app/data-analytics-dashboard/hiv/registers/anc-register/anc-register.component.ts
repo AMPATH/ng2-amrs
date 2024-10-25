ab8cf0d9a911c4f863ebb8831378b304bf1a7987
Alfred-Mutai <mutaialfr@gmail.com>
2024-10-25 12:30:26 +0300

    POC-737: Initial commit

--- src/app/data-analytics-dashboard/hiv/registers/anc-register/anc-register.component.ts
+++ src/app/data-analytics-dashboard/hiv/registers/anc-register/anc-register.component.ts
@@ -1,7 +1,18 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as _ from 'lodash';
 import * as Moment from 'moment';
+import { DataAnalyticsDashboardService } from 'src/app/data-analytics-dashboard/services/data-analytics-dashboard.services';
+import { ANCRegisterResourceService } from 'src/app/etl-api/anc-register-resource.service';
+import * as html2canvas from 'html2canvas';
+import * as jsPDF from 'jspdf';
 @Component({
   selector: 'app-anc-register',
   templateUrl: './anc-register.component.html',
@@ -12,9 +23,9 @@ export class AncRegisterComponent implements OnInit {
   public params: any;
   public indicators: string;
   public selectedIndicators = [];
-  public txnewReportSummaryData: any = [];
+  public ancRegisterData: any = [];
   public columnDefs: any = [];
-  public reportName = 'ANC Register';
+  public reportName = 'AnteNatal (ANC) Register MOH 405';
   public currentView = 'monthly';
   public currentViewBelow = 'pdf';
   public month: string;
@@ -22,16 +33,18 @@ export class AncRegisterComponent implements OnInit {
   public quarter: string;
   public eDate: string;
   public sDate: string;
+  public jointLocationUuids: string;
 
   public statusError = false;
   public errorMessage = '';
   public showInfoMessage = false;
   public isLoading = false;
   public reportHead: any;
-  public enabledControls = 'locationControl,monthControl';
+  public enabledControls = 'locationControl,dayControl';
   public pinnedBottomRowData: any = [];
   public _month: string;
   public isReleased = true;
+  @ViewChild('cntdarcontentToSnapshot') contentToSnapshot!: ElementRef;
 
   public _locationUuids: any = [];
   public get locationUuids(): Array<string> {
@@ -48,13 +61,16 @@ export class AncRegisterComponent implements OnInit {
     this._locationUuids = locationUuids;
   }
 
-  constructor(public router: Router, public route: ActivatedRoute) {
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public ancRegisterService: ANCRegisterResourceService,
+    private datePipe: DatePipe,
+    private dataAnalyticsDashboardService: DataAnalyticsDashboardService
+  ) {
     this.route.queryParams.subscribe((data) => {
       data.month === undefined
-        ? (this._month = Moment()
-            .subtract(1, 'M')
-            .endOf('month')
-            .format('YYYY-MM-DD'))
+        ? (this._month = Moment().format('YYYY-MM-DD'))
         : (this._month = data.month);
 
       this.showDraftReportAlert(this._month);
@@ -64,24 +80,30 @@ export class AncRegisterComponent implements OnInit {
   ngOnInit() {}
 
   public onMonthChange(value): any {
-    this._month = Moment(value).endOf('month').format('YYYY-MM-DD');
+    this._month = Moment(value).format('YYYY-MM-DD');
   }
 
   public generateReport(): any {
+    this.dataAnalyticsDashboardService
+      .getSelectedLocations()
+      .subscribe((data) => {
+        const locationValues = data.locations.map(
+          (location) => `'${location.value}'`
+        );
+        this.jointLocationUuids = locationValues.join(', ');
+      });
+
     this.route.parent.parent.params.subscribe((params: any) => {
-      this.storeParamsInUrl(params.location_uuid);
+      this.storeParamsInUrl();
     });
-    this.txnewReportSummaryData = [];
-    // this.getTxNewReport(this.params);
+    this.ancRegisterData = [];
+    this.getANCRegisterData(this.params);
   }
 
-  public storeParamsInUrl(param) {
+  public storeParamsInUrl() {
     this.params = {
-      locationUuids: param,
-      _month: Moment(this._month).endOf('month').format('YYYY-MM-DD'),
-      month: Moment(this._month).endOf('month').format('YYYY-MM-DD'),
-      reportName: this.reportName,
-      _date: Moment(this._month).format('DD-MM-YYYY')
+      locationUuids: this.jointLocationUuids,
+      month: Moment(this._month).format('YYYY-MM-DD')
     };
     this.router.navigate([], {
       relativeTo: this.route,
@@ -89,31 +111,31 @@ export class AncRegisterComponent implements OnInit {
     });
   }
 
-  // public getTxNewReport(params: any) {
-  //   this.isLoading = true;
-  //   this.txnewReport.getTxNewReport(params).subscribe((data) => {
-  //     if (data.error) {
-  //       this.showInfoMessage = true;
-  //       this.errorMessage = `There has been an error while loading the report, please retry again`;
-  //       this.isLoading = false;
-  //     } else {
-  //       this.showInfoMessage = false;
-  //       this.columnDefs = data.sectionDefinitions;
-  //       this.txnewReportSummaryData = data.result;
-  //       this.calculateTotalSummary();
-  //       this.isLoading = false;
-  //       this.showDraftReportAlert(this._month);
-  //     }
-  //   });
-  // }
+  public getANCRegisterData(params: any) {
+    this.isLoading = true;
+    this.ancRegisterService.getANCRegisterRegister(params).subscribe((data) => {
+      if (data.error) {
+        this.showInfoMessage = true;
+        this.errorMessage = `There has been an error while loading the report, please retry again`;
+        this.isLoading = false;
+      } else {
+        this.showInfoMessage = false;
+        this.columnDefs = data.sectionDefinitions;
+        this.ancRegisterData = data;
+        this.calculateTotalSummary();
+        this.isLoading = false;
+        this.showDraftReportAlert(this._month);
+      }
+    });
+  }
 
   public calculateTotalSummary() {
     const totalsRow = [];
-    if (this.txnewReportSummaryData.length > 0) {
+    if (this.ancRegisterData.length > 0) {
       const totalObj = {
         location: 'Totals'
       };
-      _.each(this.txnewReportSummaryData, (row) => {
+      _.each(this.ancRegisterData, (row) => {
         Object.keys(row).map((key) => {
           if (Number.isInteger(row[key]) === true) {
             if (totalObj[key]) {
@@ -152,10 +174,27 @@ export class AncRegisterComponent implements OnInit {
   }
 
   public showDraftReportAlert(date) {
-    if (date != null && date >= Moment().endOf('month').format('YYYY-MM-DD')) {
+    if (date != null && date >= Moment().format('YYYY-MM-DD')) {
       this.isReleased = false;
     } else {
       this.isReleased = true;
     }
+  }
+
+  transformDate(date: string): string | null {
+    return this.datePipe.transform(date, 'dd/MM/yyyy');
+  }
+
+  public takeSnapshotAndExport() {
+    const elementToSnapshot = this.contentToSnapshot.nativeElement;
+
+    html2canvas(elementToSnapshot).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const imgWidth = 210; // A4 width in mm
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+      pdf.save('MOH 405 ANC Register.pdf');
+    });
   }
 }
--- src/app/etl-api/anc-register-resource.service.ts
+++ src/app/etl-api/anc-register-resource.service.ts
@@ -7,7 +7,7 @@ import { catchError, map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class RegistersResourceService {
+export class ANCRegisterResourceService {
   public get url(): string {
     return this.appSettingsService.getEtlRestbaseurl().trim();
   }
@@ -15,29 +15,10 @@ export class RegistersResourceService {
     public http: HttpClient,
     public appSettingsService: AppSettingsService
   ) {}
-  public getPrEPRegisterReport(params: any): Observable<any> {
+  public getANCRegisterRegister(params: any): Observable<any> {
     return this.http
       .get(
-        `${this.url}registers/prepregisterdata?month=${params.month}&locationUuids=${params.locationUuids}`
-      )
-      .pipe(
-        catchError((err: any) => {
-          const error: any = err;
-          const errorObj = {
-            error: error.status,
-            message: error.statusText
-          };
-          return Observable.of(errorObj);
-        }),
-        map((response: Response) => {
-          return response;
-        })
-      );
-  }
-  public getDefaulterTracingRegister(params: any): Observable<any> {
-    return this.http
-      .get(
-        `${this.url}registers/defaultertracing?month=${params.month}&locationUuids=${params.locationUuids}`
+        `${this.url}registers/ancregister?endDate=${params.month}&locationUuids=${params.locationUuids}`
       )
       .pipe(
         catchError((err: any) => {
