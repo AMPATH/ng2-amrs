4ea3a2d28e8ebc41e2220343187739eaa4098381
kantush <sainingo254@gmail.com>
2025-07-07 12:49:32 +0300

    feat: Work on Medication delivery patient list

---
+++ src/app/etl-api/medication-delivery-list.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AppSettingsService } from '../app-settings/app-settings.service';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class MedicationDeliveryResourceService {
+  constructor(
+    private http: HttpClient,
+    private appSettingsService: AppSettingsService
+  ) {}
+
+  public getMedicationDeliveryUrl(): string {
+    return (
+      this.appSettingsService.getEtlRestbaseurl().trim() +
+      'medication-delivery-list'
+    );
+  }
+
+  public getMedicationDeliveryList(
+    startDate: string,
+    endDate: string
+  ): Observable<any> {
+    return this.http.get(this.getMedicationDeliveryUrl(), {
+      params: new HttpParams()
+        .set('startDate', startDate)
+        .set('endDate', endDate),
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }),
+      observe: 'body',
+      responseType: 'json'
+    });
+  }
+}
