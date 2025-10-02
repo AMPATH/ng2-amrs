b18540905de62cf6b56c8b9279df810e84ec18d1
maikofelix47 <maikofelix89@gmail.com>
2025-10-02 14:07:43 +0300

    (feat) Add Facility Registry module

--- src/app/facility-management/facility-management.module.ts
+++ src/app/facility-management/facility-management.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { FacilitySearchComponent } from './facility-search/facility-search.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { LocationResourceService } from '../openmrs-api/location-resource.service';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FacilityDetailsComponent } from './facility-details/facility-details.component';
+
+const routes: Routes = [
+  {
+    path: 'search',
+    component: FacilitySearchComponent,
+    canActivate: []
+  }
+];
 
 @NgModule({
-  imports: [],
-  declarations: [FacilitySearchComponent],
-  providers: []
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    NgSelectModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [FacilitySearchComponent, FacilityDetailsComponent],
+  providers: [LocationResourceService]
 })
 export class FacilityManagementModule {}
