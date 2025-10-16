729db5282ac9e6104f4e760e9de047bbdeea90f2
Felix Maiko <maikofelix89@gmail.com>
2025-10-16 19:20:35 +0300

    (fix) Add location uuid params in hie endpoints (#1838)
    * (fix) Add location uuid params in hie endpoints
    
    * (fix) prettier fix

--- src/app/practitioner-management/practitioner-search/practitioner-search.component.ts
+++ src/app/practitioner-management/practitioner-search/practitioner-search.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HealthInformationExchangeService } from 'src/app/hie-api/health-information-exchange.service';
-import { Practitioner, Providers } from 'src/app/models/practitioner.model';
-import { UserDefaultPropertiesService } from 'src/app/user-default-properties';
+import {
+  Practitioner,
+  PractitionerSearchParams,
+  Providers
+} from '../../models/practitioner.model';
+import { UserDefaultPropertiesService } from '../../user-default-properties';
 
 @Component({
   selector: 'app-practitioner-search',
   templateUrl: './practitioner-search.component.html',
   styleUrls: ['./practitioner-search.component.css']
 })
-export class PractitionerSearchComponent {
+export class PractitionerSearchComponent implements OnInit {
   searchForm: FormGroup;
   providers: Providers[] = [];
   allProviders: Providers[] = [];
@@ -18,16 +22,26 @@ export class PractitionerSearchComponent {
   selectedPractitioner: Practitioner | null = null;
   showModal = false;
   errorMessage: string | null = null;
+  public currentUserLocation: { uuid: string; display: string };
 
   constructor(
     private fb: FormBuilder,
     private practitionerService: HealthInformationExchangeService,
-    private propertyLocationService: UserDefaultPropertiesService
+    private propertyLocationService: UserDefaultPropertiesService,
+    private userDefaultPropertiesService: UserDefaultPropertiesService
   ) {
     this.searchForm = this.fb.group({
       searchType: ['NATIONAL_ID'],
       searchValue: ['']
     });
+  }
+
+  ngOnInit(): void {
+    this.getUserCurrentLocation();
+  }
+
+  private getUserCurrentLocation() {
+    this.currentUserLocation = this.userDefaultPropertiesService.getCurrentUserDefaultLocationObject();
   }
 
   getAllProviders(): void {
@@ -69,7 +83,9 @@ export class PractitionerSearchComponent {
 
     this.isLoading = true;
 
-    const searchParams: any = {};
+    const searchParams: any = {
+      locationUuid: this.currentUserLocation.uuid
+    };
 
     switch (searchType) {
       case 'NATIONAL_ID':
@@ -124,7 +140,10 @@ export class PractitionerSearchComponent {
 
   viewProviderDetails(provider: any): void {
     this.isLoading = true;
-    const searchParams = { nationalId: provider.national_id };
+    const searchParams: PractitionerSearchParams = {
+      nationalId: provider.national_id,
+      locationUuid: this.currentUserLocation.uuid
+    };
 
     this.practitionerService.searchPractitioners(searchParams).subscribe({
       next: (practitioners) => {
