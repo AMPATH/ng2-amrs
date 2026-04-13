328150b1ec29d70d5cfd3c803d782df55bc5a21f
Denzel Kipkemoi <denzelkipkemoi@Denzels-MacBook-Pro.local>
2026-04-13 12:07:50 +0300

    fix: fixed age friendly modal

--- src/app/patient-dashboard/common/formentry/confirm-modal.component.ts
+++ src/app/patient-dashboard/common/formentry/confirm-modal.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'confirm-modal',
   templateUrl: './confirm-modal.component.html'
 })
-export class ConfirmModalComponent {
+export class ConfirmModalComponent implements OnInit, OnDestroy {
   title = 'Confirm';
   message =
     'The patient is 50 years and above. Would you like to proceed and fill the age friendly tool?';
@@ -13,9 +14,32 @@ export class ConfirmModalComponent {
   onConfirm!: () => void;
   onCancel!: () => void;
 
-  constructor(public bsModalRef: BsModalRef) {}
+  private confirmed = false;
+  private sub!: Subscription;
+
+  constructor(
+    public bsModalRef: BsModalRef,
+    private modalService: BsModalService
+  ) {}
+
+  ngOnInit() {
+    this.sub = this.modalService.onHidden.subscribe(() => {
+      if (!this.confirmed) {
+        if (this.onCancel) {
+          this.onCancel();
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 
   confirm() {
+    this.confirmed = true;
     if (this.onConfirm) {
       this.onConfirm();
     }
